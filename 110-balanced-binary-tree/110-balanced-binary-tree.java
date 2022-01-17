ab2d42ca4183514faae746dc79dda24fc963af5e
mohit9494 <sanghaimohit100@gmail.com>
2022-01-16 21:41:28 -0500

    Time: 1 ms (65.10%), Space: 41.8 MB (25.74%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -14,28 +14,36 @@
  * }
  */
 class Solution {
-
     
-    private int depth(TreeNode root) {
+    private int heightHelper(TreeNode root) {
         
-        if(root == null) return 0;
+       if (root == null) return 0 ;
         
-        int ld = depth(root.left);
-        if(ld == -1) return -1;
-        
-        int rd = depth(root.right);
-        if(rd == -1) return -1;
-        
-        if(Math.abs(ld - rd) > 1) return -1;
-            
-        return 1 + Math.max(ld, rd);
-        
-    }
+       int lh = heightHelper(root.left);
+        int rh = heightHelper(root.right);
+         
+        return  Math.max(lh,rh) + 1;
+}
     
     public boolean isBalanced(TreeNode root) {
         
-        // Tree is balanced if it doen't have -1 depth
-        return depth(root) != -1;
+        
+        
+       if(root == null) return true; 
+        
+        int lh = heightHelper(root.left);
+        int rh = heightHelper(root.right);
+        
+        if(Math.abs(lh - rh) > 1) return false;
+        
+        boolean l = isBalanced(root.left);
+        boolean r = isBalanced(root.right);
+        
+        if(!l || !r) return false;
+        else return true;
+        
         
     }
+    
+    
 }

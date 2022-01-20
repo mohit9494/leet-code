83f204c9e5360ade0797154b0b9adef1bfeb69cd
mohit9494 <sanghaimohit100@gmail.com>
2022-01-20 02:30:15 -0500

    Time: 3 ms (9.53%), Space: 39.1 MB (25.79%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -27,7 +27,32 @@ class Solution {
         
         if(root == null) return true;
         
-        return isMirror(root.left, root.right);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        
+        while(!q.isEmpty()){
+         
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            
+            if(t1 == null && t2 == null) continue;
+            if(t1 == null || t2 == null) return false;
+            if(t1.val != t2.val) return false;
+            
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+            
+            
+        }
+        
+        return true;
+        
+       // return isMirror(root.left, root.right);
+        
+        
         
     }
 }

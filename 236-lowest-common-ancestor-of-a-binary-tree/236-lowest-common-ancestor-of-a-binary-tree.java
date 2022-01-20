ed1ee54c0edb9209cbd7c039bc6c3a4ff2317ee0
mohit9494 <sanghaimohit100@gmail.com>
2022-01-20 18:22:41 -0500

    Time: 19 ms (9.11%), Space: 46.3 MB (5.33%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -31,10 +31,42 @@ class Solution {
         
     }
     
+    public boolean getPath(TreeNode root, TreeNode p, List<TreeNode> path) {
+        
+        if(root == null) return false;
+        
+        path.add(root);
+        
+        if(root.val == p.val) return true;
+        
+        if(getPath(root.left, p, path) || getPath(root.right, p, path)) return true;
+        
+        path.remove(path.size() - 1) ;         
+        return false;
+        
+    }
+    
     
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        return helper(root, p , q);
+     List<TreeNode> path1 = new ArrayList<>();
+    List<TreeNode> path2 = new ArrayList<>();
         
+        getPath(root,p,path1);
+        getPath(root, q, path2);
+        
+                
+        int min = path1.size() > path2.size() ? path2.size() :path1.size();
+        List<TreeNode> ans  = new ArrayList<>();
+        
+        for(int i = 0; i < min; i++) {
+            if (path1.get(i).val == path2.get(i).val) {
+                ans.add(path1.get(i));
+            } else {
+                break;
+            }
+        }
+        
+        return ans.get(ans.size() - 1); 
     }
 }

bd39bb3923d92e5a3a7254a83faca83848c2c778
mohit9494 <sanghaimohit100@gmail.com>
2022-01-22 00:41:53 -0500

    Time: 2 ms (10.72%), Space: 39.7 MB (17.35%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -18,7 +18,7 @@ class Solution {
     TreeNode prev = null;
     
     public void flatten(TreeNode root) {
-        
+      /*  
         // 1 => Recursive   // Go right -> RLN
             if(root == null) return ;
         
@@ -27,7 +27,29 @@ class Solution {
             
             root.right = prev;
             root.left = null;
-            prev = root;
+            prev = root;*/
+        
+        //2 using Stacks
+        if(root == null) return ;
+        
+        Stack<TreeNode> st = new Stack();
+        st.add(root);
+        
+        while(!st.isEmpty()) {
+            
+            TreeNode node = st.pop();
+            
+            if(node.right != null) st.add(node.right);
+            if(node.left != null) st.add(node.left);
+            
+            if(!st.isEmpty()) {
+                node.right = st.peek();
+                node.left = null;
+            }
+            
+            
+        }
+        
      
     }
 }

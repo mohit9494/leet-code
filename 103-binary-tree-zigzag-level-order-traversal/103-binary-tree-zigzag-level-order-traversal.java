d5a2e378fca1527dfdfaea7744ea322217c76dd4
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-01-28 01:46:48 -0500

    Time: 2 ms (34.39%), Space: 42.8 MB (7.87%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -14,39 +14,66 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    
+    
+    List<List<Integer>> result = new ArrayList();
+    
+    public void helper(TreeNode root, int level) {
         
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        
-        if(root == null) return result;
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        boolean zigZagFlag = false;
-        
-        while(!q.isEmpty()) {
-            
-           LinkedList<Integer> subList = new LinkedList<>();
-           int size = q.size(); 
-            
-            for(int i = 0; i< size; i++) {
-                
-                TreeNode temp = q.poll();
-                
-                if(temp.left != null) q.add(temp.left);
-                if(temp.right != null) q.add(temp.right);
-                
-                if(zigZagFlag) subList.addFirst(temp.val);
-                else subList.add(temp.val);
-            }
-            zigZagFlag = !zigZagFlag;
-            result.add(subList);
-            
+        if(level == result.size()) {
+            LinkedList<Integer> sub = new LinkedList();
+            result.add(sub);
         }
         
+        if(level % 2 == 0) {
+            result.get(level).add(root.val);
+        }else {
+            result.get(level).add(0,root.val);
+        }
         
+        if(root.left != null) helper(root.left, level + 1);
+        if(root.right != null) helper(root.right, level + 1);
+        
+    }
+    
+    public void bfs(TreeNode root) {
+        
+      Queue<TreeNode> q = new LinkedList();
+      q.add(root);
+      int level = 0;
+        
+      while(!q.isEmpty()) {
+          
+          int size = q.size();
+          List<Integer> sub = new ArrayList();
+          
+          for(int i = 0; i< size; i++) {
+              
+           TreeNode node  = q.poll();
+           if(level % 2 == 0 ){
+             sub.add(node.val);  
+           } else {
+               sub.add(0, node.val);
+           }
+           
+              
+           if(node.left != null) q.add(node.left);   
+            if(node.right != null) q.add(node.right);
+              
+              
+          }
+          result.add(sub);
+          level++;
+      }  
+        
+    }
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        if(root == null) return result;
+        bfs(root);
+       // helper(root,0);
         return result;
-        
         
     }
 }

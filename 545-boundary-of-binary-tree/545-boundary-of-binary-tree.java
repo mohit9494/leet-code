89807efc86965385dea7073f519b3a24fa76e052
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-01-28 03:31:27 -0500

    Time: 2 ms (50.32%), Space: 45.2 MB (7.91%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.java
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.java
@@ -15,53 +15,56 @@
  */
 class Solution {
     
-    List<Integer> result = new LinkedList<>();
+    List<Integer> result = new ArrayList();
     
-    private boolean isLeaf(TreeNode t) {
-        return t.left == null && t.right == null;
+    public boolean isLeaf(TreeNode root) {
+        return root.left == null && root.right == null;
     }
     
-    private void addLeaves(TreeNode root) {
+    public void addLeftBoundry(TreeNode root){
+        
+        if(root == null) return;
+        
+        while(!isLeaf(root)) {
+            
+            result.add(root.val);
+            
+            if(root.left!= null) root = root.left;
+            else root = root.right;
+            
+        }
+        
+    }
+    
+        public void addRightBoundry(TreeNode root){
+        
+        if(root == null) return;
+            
+        List<Integer> tmp = new ArrayList();
+        
+        while(!isLeaf(root)) {
+            
+            tmp.add(0,root.val);
+            
+            if(root.right!= null) root = root.right;
+            else root = root.left;
+            
+        }
+            
+         for(int i : tmp) result.add(i);   
+        
+    }
+    
+    public void addLeaves(TreeNode root) {
+        
+        if(root == null) return;
         
         if(isLeaf(root)) {
             result.add(root.val);
-        } else {
-            if(root.left != null) addLeaves(root.left);
-            
-            if(root.right != null) addLeaves(root.right);
         }
         
-    }
-    
-    private void addLeftBoundry(TreeNode root) {
-        
-        TreeNode curr = root.left;
-        
-        while(curr != null) {
-            
-            if(!isLeaf(curr)) result.add(curr.val);
-            
-            if(curr.left != null) curr = curr.left;
-            else curr = curr.right;
-        }
-        
-        
-    } 
-    
-    private void addRightBoundry(TreeNode root){
-        
-        TreeNode curr = root.right;
-        List<Integer> tmp = new ArrayList<>();
-        
-        while(curr != null) {
-            
-            if(!isLeaf(curr)) tmp.add(curr.val);
-            
-            if(curr.right != null) curr = curr.right;
-            else curr = curr.left;
-        }
-        
-        for(int i = tmp.size() - 1; i >=0 ; i--) result.add(tmp.get(i));
+        addLeaves(root.left);
+        addLeaves(root.right);
         
     }
     
@@ -71,9 +74,9 @@ class Solution {
         
         if(!isLeaf(root)) result.add(root.val);
         
-        addLeftBoundry(root);
+        addLeftBoundry(root.left);
         addLeaves(root);
-        addRightBoundry(root);
+        addRightBoundry(root.right);
         
         return result;
         

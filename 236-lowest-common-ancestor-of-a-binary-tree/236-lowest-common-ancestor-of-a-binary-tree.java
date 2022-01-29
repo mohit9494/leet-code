2838871210ff14c7571498b0b652ca5868673dda
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-01-28 19:30:54 -0500

    Time: 17 ms (13.44%), Space: 48.1 MB (5.19%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,64 +9,45 @@
  */
 class Solution {
     
-    // This is flag problem
-    
-    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
-        
-     if (root == null) return null;   
-        
-     if (root == p || root == q) return root;
-       
-     TreeNode lh = helper(root.left, p,q); 
-        
-        TreeNode rh = helper(root.right, p,q);
-        
-        if(lh == null && rh == null) return null;
-        
-        if(lh != null && rh != null) return root;
-        
-        if(lh != null) return lh;
-        else return rh;
-        
-        
-    }
-    
-    public boolean getPath(TreeNode root, TreeNode p, List<TreeNode> path) {
+    private boolean getPath(TreeNode root, TreeNode t, List<TreeNode> path){
         
         if(root == null) return false;
         
         path.add(root);
         
-        if(root.val == p.val) return true;
+        if(root.val == t.val) return true;
         
-        if(getPath(root.left, p, path) || getPath(root.right, p, path)) return true;
+        boolean lf = getPath(root.left, t, path);
+        boolean rf = getPath(root.right, t, path);
         
-        path.remove(path.size() - 1) ;         
+        if(lf || rf) return true;
+        
+        path.remove(path.size() - 1);
         return false;
         
     }
     
-    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-     List<TreeNode> path1 = new ArrayList<>();
-    List<TreeNode> path2 = new ArrayList<>();
+        //Using Brute Force
+        List<TreeNode> path1 = new ArrayList();
+        List<TreeNode> path2 = new ArrayList();
         
-        getPath(root,p,path1);
+        getPath(root, p, path1);
         getPath(root, q, path2);
         
-                
-        int min = path1.size() > path2.size() ? path2.size() :path1.size();
-        List<TreeNode> ans  = new ArrayList<>();
+        int min = path1.size() > path2.size() ? path2.size() : path1.size();
+        
+        List<TreeNode> ans = new ArrayList();
         
         for(int i = 0; i < min; i++) {
-            if (path1.get(i).val == path2.get(i).val) {
-                ans.add(path1.get(i));
-            } else {
-                break;
-            }
+            
+            if(path1.get(i).val == path2.get(i).val) ans.add(path1.get(i));
+            else break;
+            
         }
         
-        return ans.get(ans.size() - 1); 
+        return ans.get(ans.size() - 1);
+        
     }
 }

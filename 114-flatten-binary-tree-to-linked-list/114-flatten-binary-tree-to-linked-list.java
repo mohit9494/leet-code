feac6c731b5c9d8155a1bd415c2fcf4182ba3511
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-02-03 13:52:53 -0500

    Time: 1 ms (59.70%), Space: 42.7 MB (5.31%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,27 +14,43 @@
  * }
  */
 class Solution {
+    
+    TreeNode prev = null;
+    
     public void flatten(TreeNode root) {
         
-        // 1 -> Using Stack -> Like preorder
+        // 2 -> Using Recursion
+        // -> RLN -> MOVE Left connection to right
         
         if(root == null) return;
         
-        Stack<TreeNode> st = new Stack();
-        st.add(root);
+        flatten(root.right);
+        flatten(root.left);
         
-        while(!st.isEmpty()) {
+        root.right = prev;
+        root.left = null;
+        prev = root;
+        
+        // 1 -> Using Stack -> Like preorder
+        
+//         if(root == null) return;
+        
+//         Stack<TreeNode> st = new Stack();
+//         st.add(root);
+        
+//         while(!st.isEmpty()) {
             
-            TreeNode node = st.pop();
+//             TreeNode node = st.pop();
             
-            if(node.right != null) st.add(node.right);
-            if(node.left != null) st.add(node.left);
+//             if(node.right != null) st.add(node.right);
+//             if(node.left != null) st.add(node.left);
             
-            if (!st.isEmpty()) node.right = st.peek();
+//             if (!st.isEmpty()) node.right = st.peek();
             
-            node.left = null;
+//             node.left = null;
             
-        }
+//         }
+        
         
    
     }

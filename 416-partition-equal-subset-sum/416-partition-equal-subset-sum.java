87b513b4481d821fed00a61ffc6f0a02a9e305d1
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-02 23:47:40 -0500

    Time: 44 ms (63.82%), Space: 70.7 MB (21.50%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,46 +1,41 @@
 class Solution {
     
-    public boolean isSubsetSum(int[] nums, int n, int sum, Boolean[][] t) {
-     
+    private boolean isSubsetSum(int[] nums, int sum, int n, Boolean[][] t){
+        
+        // If sum is 0 => all true
         if(sum == 0) return true;
         
-        if(n == 0 && sum > 0) return false;
+        // If n == 0 ans sum > 0 then all false
+        if (n == 0 && sum > 0) return false;
         
-        if (t[n][sum] != null) {
-            return t[n][sum];}
+        if (t[n][sum] != null) return t[n][sum];
         
         if(nums[n-1] > sum) {
-            return t[n][sum] = isSubsetSum(nums, n-1, sum, t);
-        } else  {
-            return t[n][sum] = isSubsetSum(nums, n-1, sum - nums[n-1], t) || isSubsetSum(nums, n-1, sum, t);
-        }
-        
-        
+            return t[n][sum] = isSubsetSum(nums, sum, n-1, t);
+        } else {
+            return t[n][sum] = isSubsetSum(nums, sum - nums[n-1], n-1, t) || isSubsetSum(nums, sum, n-1, t);
+        }      
+    
     }
     
     public boolean canPartition(int[] nums) {
-     
-        // Equal sum subset -> Extension of Subset sum
         
-        // If two partition with sum s - there then total sum of array = s + s
         
-        // Find sum of entire array
+        // take sum -> check if odd or even 
         int totalSum = 0;
         
-        for(int num : nums) {
+        for(int num : nums){
             totalSum += num;
         }
         
-        // Check if odd or evem
+        if (totalSum % 2 != 0) return false;
         
-        if(totalSum % 2 != 0) return false;
-        
-        int sum = totalSum / 2;
+        int sum = totalSum/2;
         int n = nums.length;
         
         Boolean[][] t = new Boolean[n+1][sum+1];
         
-        return isSubsetSum(nums, n, sum, t);
-        
+        return isSubsetSum(nums, sum, n, t);        
+    
     }
 }

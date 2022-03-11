d3aa72ebaaa9213b6a2f6880328e2c892cc80c97
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-10 19:41:55 -0500

    Time: 20 ms (71.01%), Space: 52 MB (22.32%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,16 +1,15 @@
 class Solution {
-     
-    int count = 0;
+    
     int total;
-
     
     public int calculate1(int[] nums, int target, int idx, int sum) {
         
         // reached end of array
         if(idx == nums.length) {
-            // if (sum == target) count++;
+            
             if(sum == target) return 1;
            else return 0;
+            
         } else {
             
           return calculate1(nums, target, idx + 1, sum + nums[idx]) + 
@@ -19,35 +18,36 @@ class Solution {
     }
     
         
-//     public int calcuate2(int[] nums, int target, int idx, int sum, int[][] t) {
+    public int calculate2(int[] nums, int target, int idx, int sum, int[][] t) {
         
-       
-//        if(idx == nums.length) {
-           
-//            if(sum == S) return 1;
-//            else return 0;
-           
-//        } 
+    if(idx == nums.length) {
+        if(sum == target) return 1;
+        else return 0;
+    } else {
         
+        // check for values
+        if(t[idx][total + sum] != Integer.MIN_VALUE) 
+        {
+            return t[idx][total + sum];
+        }
         
-        
-        
-        
-  //  }
+        return t[idx][total + sum] = calculate2(nums, target, idx + 1, sum + nums[idx], t) + 
+            calculate2(nums, target, idx + 1, sum - nums[idx], t);        
+    } 
+    
+    }
     
     
     public int findTargetSumWays(int[] nums, int target) {
        
-//         total = Arrays.stream(nums).sum();
+        total = Arrays.stream(nums).sum();
         
-//         int[][] t = new int[nums.length + 1][2 * total + 1];
+        int[][] t = new int[nums.length + 1][2 * total + 1];
         
-//         for(int[] k : t) Arrays.fill(k, Integer.MIN_VALUE);
+        for(int[] k : t) Arrays.fill(k, Integer.MIN_VALUE);
         
-        // calculate2(nums, target, 0, 0, t);
-        return calculate1(nums, target, 0, 0);
-            
-        // return count;
+        return calculate2(nums, target, 0, 0, t);
+        // return calculate1(nums, target, 0, 0);
         
     }
 }

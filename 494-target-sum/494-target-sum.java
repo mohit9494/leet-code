c82c7c25babaa2c72f05c990489a952c59de0d23
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-10 19:19:28 -0500

    Time: 727 ms (20.65%), Space: 42.4 MB (47.71%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,26 +1,53 @@
 class Solution {
      
     int count = 0;
+    int total;
+
     
-    public void calculate1(int[] nums, int target, int idx, int sum) {
+    public int calculate1(int[] nums, int target, int idx, int sum) {
         
         // reached end of array
         if(idx == nums.length) {
-            if (sum == target) count++;
+            // if (sum == target) count++;
+            if(sum == target) return 1;
+           else return 0;
         } else {
             
-          calculate1(nums, target, idx + 1, sum + nums[idx]);  
+          return calculate1(nums, target, idx + 1, sum + nums[idx]) + 
           calculate1(nums, target, idx + 1, sum - nums[idx]);
         }
     }
     
+        
+//     public int calcuate2(int[] nums, int target, int idx, int sum, int[][] t) {
+        
+       
+//        if(idx == nums.length) {
+           
+//            if(sum == S) return 1;
+//            else return 0;
+           
+//        } 
+        
+        
+        
+        
+        
+  //  }
+    
     
     public int findTargetSumWays(int[] nums, int target) {
        
-
-         calculate1(nums, target, 0, 0);
+//         total = Arrays.stream(nums).sum();
+        
+//         int[][] t = new int[nums.length + 1][2 * total + 1];
+        
+//         for(int[] k : t) Arrays.fill(k, Integer.MIN_VALUE);
+        
+        // calculate2(nums, target, 0, 0, t);
+        return calculate1(nums, target, 0, 0);
             
-        return count;
+        // return count;
         
     }
 }

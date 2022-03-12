bb6665732a3d102c12a2dfde5062d2834b4c4b29
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-11 21:48:14 -0500

    Time: 18 ms (74.01%), Space: 52.4 MB (18.91%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -37,10 +37,43 @@ class Solution {
     
     }
     
+//     public int helper(int[] nums, int n, int sum) {
+        
+//         int[][] t = new int[n+1][sum+1];
+        
+//         // sum = 0
+//         for(int i = 0; i < n+1 ; i++) t[i][0] = 1;
+        
+//         for(int j = 1; j < sum + 1; j++) t[0][j] = 0;
+        
+//         for(int i = 1; i < n+1; i++) {
+//             for(int j = 1; j < sum + 1; j++){
+                
+//                 if(nums[i-1] > j) {
+//                     t[i][j] = t[i-1][j];
+                    
+//                 }else {
+//                     t[i][j] = t[i-1][j] + t[i-1][j - nums[i-1]];
+//                 }
+                
+//             }
+//         }
+        
+//         return t[n][sum];
+//     }
     
     public int findTargetSumWays(int[] nums, int target) {
-       
-        total = Arrays.stream(nums).sum();
+        
+//         if(nums.length == 1) {
+//             if (nums[0] == Math.abs(target)) return 1;
+//             else return 0;
+//         }
+
+           total = Arrays.stream(nums).sum();
+        
+//         int sum = (total + target)/2;
+            
+//         return helper(nums, nums.length, sum);    
         
         int[][] t = new int[nums.length + 1][2 * total + 1];
         

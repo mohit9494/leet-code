5c7ff3e044c6dd5bd7c963050509438f1d16f171
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-27 18:20:23 -0400

    Time: 11 ms (74.30%), Space: 47.5 MB (68.37%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -5,44 +5,30 @@ class Solution {
         
         if(p.length() > s.length()) return result;
         
-        // Fill the map with p
-        Map<Character, Integer> pMap = new HashMap();
+        //using arrays
+        int[] sCount =new int[26];
+        int[] pCount = new int[26];
         
-        for(char c : p.toCharArray()) {
-            pMap.put(c, pMap.getOrDefault(c,0) + 1);
-        }
-        
+        for(char c : p.toCharArray()) pCount[c - 'a']++;
         
         int i = 0, j = 0;
-        Map<Character, Integer> sMap = new HashMap();
         
         while(j < s.length()) {
             
-            // process with J
-            char c = s.charAt(j);
-            if(sMap.containsKey(c)) {
-                sMap.put(c, sMap.get(c) + 1);
-            } else {
-                sMap.put(c,1);
-            }
+            // Initial with J
+            char cs = s.charAt(j);
             
-            if (j -i +1 < p.length()) {
+            sCount[cs - 'a']++;
+            
+            if (j - i + 1 < p.length()) {
                 j++;
-            } else if (j - i + 1 == p.length()) {
+            } else  if (j - i + 1 == p.length()) {
                 
-                //calculate
-                if(pMap.equals(sMap)) result.add(i);
+                // calculation
+                if(Arrays.equals(sCount, pCount)) result.add(i);
                 
-                // slide the window -> remove ith char
-                char ch = s.charAt(i);
-                
-                   if (sMap.get(ch) == 1) {
-                    sMap.remove(ch);
-                   }
-        else {
-          sMap.put(ch, sMap.get(ch) - 1);
-        }
-                
+                //slide the window
+                sCount[s.charAt(i) - 'a']--;
                 i++;
                 j++;
             }
@@ -50,5 +36,51 @@ class Solution {
         }
         
         return result;
+        
+//         // Fill the map with p
+//         Map<Character, Integer> pMap = new HashMap();
+        
+//         for(char c : p.toCharArray()) {
+//             pMap.put(c, pMap.getOrDefault(c,0) + 1);
+//         }
+        
+        
+//         int i = 0, j = 0;
+//         Map<Character, Integer> sMap = new HashMap();
+        
+//         while(j < s.length()) {
+            
+//             // process with J
+//             char c = s.charAt(j);
+//             if(sMap.containsKey(c)) {
+//                 sMap.put(c, sMap.get(c) + 1);
+//             } else {
+//                 sMap.put(c,1);
+//             }
+            
+//             if (j -i +1 < p.length()) {
+//                 j++;
+//             } else if (j - i + 1 == p.length()) {
+                
+//                 //calculate
+//                 if(pMap.equals(sMap)) result.add(i);
+                
+//                 // slide the window -> remove ith char
+//                 char ch = s.charAt(i);
+                
+//                 if (sMap.get(ch) == 1) {
+//                     sMap.remove(ch);
+//                    }
+//                 else {
+//                 sMap.put(ch, sMap.get(ch) - 1);
+//                     }
+                
+//                 i++;
+//                 j++;
+//             }
+ 
+//         }
+        
+//         return result;
     }
 }

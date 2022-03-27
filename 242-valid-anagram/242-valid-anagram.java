b678a37dd983ea51c0d56ef239edfda95ec6efac
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-27 15:51:52 -0400

    Time: 4 ms (79.25%), Space: 43.8 MB (50.96%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,13 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        //Hashing or arrays
+        if(s.length() != t.length()) return false;
+        
+        int[] table  = new int[26];
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            table[s.charAt(i) - 'a']++;
+            table[t.charAt(i) - 'a']--;
+        }
+        
+        for(int count : table) {
+            if(count != 0) return false;
+        }
+        
+        return true;
+        
         // sorting
-        char[] s1 = s.toCharArray();
-        char[] s2 = t.toCharArray();
+//         char[] s1 = s.toCharArray();
+//         char[] s2 = t.toCharArray();
         
-        Arrays.sort(s1);
-        Arrays.sort(s2);
+//         Arrays.sort(s1);
+//         Arrays.sort(s2);
         
-        return Arrays.equals(s1, s2);
+//         return Arrays.equals(s1, s2);
     }
 }

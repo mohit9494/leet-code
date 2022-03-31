91909e335f50094d8f600c928715749396093415
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-31 00:45:20 -0400

    Time: 8 ms (18.20%), Space: 51.8 MB (14.65%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+    // Sorting
+    Arrays.sort(nums);
+    
+    // Ensure the first and last number
+    if(nums[nums.length - 1] != nums.length) return nums.length;
+    
+    if (nums[0] != 0) return 0;
+        
+    // Now the missing number is in the range (1, n-1)
+    for(int i = 0; i < nums.length; i++) {
+        if (nums[i] != i) {
+            return i;
+        }
+    }
+        return -1;
+    }
+}

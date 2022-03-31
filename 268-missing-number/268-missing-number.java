f0ba2b4d8a7fa142ec6c612f305abf147c0839e2
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-03-31 00:46:54 -0400

    Time: 9 ms (14.81%), Space: 51.5 MB (30.86%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -5,10 +5,9 @@ class Solution {
     Arrays.sort(nums);
     
     // Ensure the first and last number
-    if(nums[nums.length - 1] != nums.length) return nums.length;
-    
     if (nums[0] != 0) return 0;
-        
+    if(nums[nums.length - 1] != nums.length) return nums.length;
+ 
     // Now the missing number is in the range (1, n-1)
     for(int i = 0; i < nums.length; i++) {
         if (nums[i] != i) {

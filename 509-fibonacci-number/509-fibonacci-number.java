739e09624804f2913c5baa7210b09fb20ba7630e
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-04 15:14:48 -0400

    Time: 1 ms (43.68%), Space: 41.7 MB (7.91%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,5 +1,7 @@
 class Solution {
     
+    private Map<Integer, Integer> map = new HashMap<>(Map.of(0,0,1,1));
+    
     public int fib(int n) {
      
     if (n <= 1 ) return n;
@@ -7,15 +9,22 @@ class Solution {
     //return fib(n-1) + fib(n-2);
     
     // cache Tabulation
-    int[] cache = new int[n+1];
-    cache[1] = 1;
+//     int[] cache = new int[n+1];
+//     cache[1] = 1;
     
-    for(int i = 2; i <= n; i++) {
-        cache[i] = cache[i - 1] + cache[i - 2];
-    }
+//     for(int i = 2; i <= n; i++) {
+//         cache[i] = cache[i - 1] + cache[i - 2];
+//     }
     
-    return cache[n];
+//     return cache[n];
         
+        // Using Memoization
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
         
+        map.put(n, fib(n-1) + fib(n-2));
+        
+        return map.get(n);
     }
 }

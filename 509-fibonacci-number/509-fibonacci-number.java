16ad7e70f89d6b71cdc4b3fd5c6d8ca18ff5a68e
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-04 15:09:53 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (53.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,13 +1,20 @@
 class Solution {
+    
     public int fib(int n) {
-        
-        
-        
-        if(n <= 1) return n;
-        
-        return fib(n-1) + fib(n-2);
-        
-        
+     
+    if (n <= 1 ) return n;
+    // worst
+    //return fib(n-1) + fib(n-2);
+    
+    // cache Tabulation
+    int[] cache = new int[n+1];
+    cache[1] = 1;
+    
+    for(int i = 2; i <= n; i++) {
+        cache[i] = cache[i - 1] + cache[i - 2];
+    }
+    
+    return cache[n];
         
         
     }

4a1decdc2a56decbe2c75d649c2883c1445734ec
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-08 18:38:55 -0400

    Time: 0 ms (100.00%), Space: 42.4 MB (85.16%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -15,37 +15,60 @@
  */
 class Solution {
     
-    List<List<Integer>> ans = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    
+    private void helper(TreeNode node, int level){
+        
+        if(ans.size() == level) {
+            ans.add(new ArrayList<Integer>());
+        }
+        
+        ans.get(level).add(node.val);
+        
+        if(node.left!= null) helper(node.left, level + 1);
+        if(node.right!= null) helper(node.right, level + 1);
+        
+        
+    }
+    
     
     public List<List<Integer>> levelOrder(TreeNode root) {
         
-        // BFS Using Iterative Queue
+        // BFS Using DFS
+        if(root == null) return ans;
         
-        if (root == null) return ans;
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        while(!q.isEmpty()) {
-            
-            int size = q.size();
-            List<Integer> subList = new ArrayList<>();
-            
-            for(int i = 0; i < size; i++){
-                
-                TreeNode temp = q.poll();
-                subList.add(temp.val);
-                
-                if (temp.left != null) q.add(temp.left);
-                if(temp.right != null) q.add(temp.right);
-                
-                
-            }
-            
-            ans.add(subList);
-            
-        }
+        helper(root, 0);
         
         return ans;
+        
+        
+        // BFS Using Iterative Queue
+        
+//         if (root == null) return ans;
+        
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+        
+//         while(!q.isEmpty()) {
+            
+//             int size = q.size();
+//             List<Integer> subList = new ArrayList<>();
+            
+//             for(int i = 0; i < size; i++){
+                
+//                 TreeNode temp = q.poll();
+//                 subList.add(temp.val);
+                
+//                 if (temp.left != null) q.add(temp.left);
+//                 if(temp.right != null) q.add(temp.right);
+                
+                
+//             }
+            
+//             ans.add(subList);
+            
+//         }
+        
+//         return ans;
     }
 }

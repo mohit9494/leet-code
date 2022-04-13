191837ee8426f68333c20854af2eebc0509f7b85
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-13 05:38:40 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (89.19%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,44 +15,20 @@
  */
 class Solution {
     
-    public boolean isMirror(TreeNode left, TreeNode right){
+    private boolean isMirror(TreeNode left, TreeNode right) {
         
         if(left == null || right == null) return left == right;
         
-        return (left.val == right.val)  && isMirror(left.left, right.right) &&                            isMirror(left.right, right.left);
-}
-    
+        return (left.val == right.val && isMirror(left.left, right.right) && 
+                isMirror(left.right, right.left));
+        
+    }
     
     public boolean isSymmetric(TreeNode root) {
         
         if(root == null) return true;
         
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        q.add(root);
-        
-        while(!q.isEmpty()){
-         
-            TreeNode t1 = q.poll();
-            TreeNode t2 = q.poll();
-            
-            if(t1 == null && t2 == null) continue;
-            if(t1 == null || t2 == null) return false;
-            if(t1.val != t2.val) return false;
-            
-            q.add(t1.left);
-            q.add(t2.right);
-            q.add(t1.right);
-            q.add(t2.left);
-            
-            
-        }
-        
-        return true;
-        
-       // return isMirror(root.left, root.right);
-        
-        
+        return isMirror(root.left, root.right);
         
     }
 }

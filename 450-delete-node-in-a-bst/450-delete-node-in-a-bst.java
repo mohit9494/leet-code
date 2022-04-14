4acb500dee6eb2030abf60dfaf9415132e0e7282
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-14 17:56:23 -0400

    Time: 0 ms (100.00%), Space: 42.9 MB (81.32%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -15,34 +15,55 @@
  */
 class Solution {
     
-    private TreeNode findMin(TreeNode node) {
+    // Find the successor
+    public int getS(TreeNode root) {
+        root = root.right;
         
-        while(node.left != null) node = node.left;
-        return node;
-        
+        while(root.left != null) {
+            root = root.left;
+        }
+        return root.val;
     }
+    
+    // Find the predecessor
+    public int getP(TreeNode root) {
+        root = root.left;
+        
+        while(root.right != null) {
+            root = root.right;
+        }
+        
+        return root.val;
+    }
+    
     public TreeNode deleteNode(TreeNode root, int key) {
         
         if(root == null) return null;
         
-        if(key < root.val) {
-            root.left = deleteNode(root.left, key);
-        } else if(key > root.val){
+        // Deleted Node is on the right-> move to right
+        if(root.val < key) {
             root.right = deleteNode(root.right, key);
-        } else  {
-            
-            if(root.left == null) return root.right;
-            else if (root.right == null) return root.left;
-            
-            // If both Child Present -> Go to right ->find min and replace and delete
-            
-            TreeNode minNode = findMin(root.right);
-            root.val = minNode.val;
-            
-            root.right = deleteNode(root.right, minNode.val);
-     
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            // We found the nod eto delete
+            // If it is a lea node::
+            if(root.left == null && root.right == null) {
+                root = null;
+            } else if(root.left == null) {
+                // Move to right then , replace value with successor and then delete successor
+                root.val = getS(root);
+                // delete the successor by going to right
+                root.right = deleteNode(root.right, root.val);
+            } else {
+                // root has no right -> replace the root val with predecessor
+                root.val = getP(root);
+                root.left = deleteNode(root.left, root.val);
+                
+            }
         }
         
         return root;
+        
     }
 }

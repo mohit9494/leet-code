1a0deb00d8278b0d79e0c14162948ae27619c889
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-14 18:52:33 -0400

    Time: 4 ms (100.00%), Space: 43.5 MB (83.40%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -15,11 +15,17 @@ class Solution {
         
         if(root == null) return null;
         
-        if(root.val > p.val && root.val > q.val) {
-            return lowestCommonAncestor(root.left, p , q);
-        } else if (root.val < p.val && root.val < q.val) {
-            return  lowestCommonAncestor(root.right, p , q);
-        } 
+        // if(root.val > p.val && root.val > q.val) {
+        //     return lowestCommonAncestor(root.left, p , q);
+        // } else if (root.val < p.val && root.val < q.val) {
+        //     return  lowestCommonAncestor(root.right, p , q);
+        // } 
+        
+        while(root != null) {
+           if(root.val > p.val && root.val > q.val) root = root.left;
+            else if (root.val < p.val && root.val < q.val) root = root.right;
+            else return root;
+        }
         
         return root;
         

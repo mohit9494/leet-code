8590bae9aebd6a057fcff7a707be1ae8f4c4a379
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-16 19:07:31 -0400

    Time: 7 ms (84.02%), Space: 42.3 MB (90.53%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
@@ -1,39 +1,42 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-    
-        if(s == null || s.length() == 0 || k == 0 ) return 0;
+        
+        // This is a classis window problen
+        // Variable window with at most k
+        
+        if( k == 0 ) return 0;
         
         int i = 0;
         int j = 0;
-        int maxi = 0 ;
+        int maxi = 0;
         
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap();
         
-        while(j < s.length()) {
+        while (j < s.length()) {
             
-            char c = s.charAt(j);
-            map.put(c, map.getOrDefault(c , 0) + 1);
+            char c = s.charAt(j);            
+            map.put(c, map.getOrDefault(c, 0) + 1);
             
-            while (map.size() > k ) {
-                
-                char d = s.charAt(i);
-                
-                int frequency = map.get(d);
-                
-                if(frequency == 1) {
-                    map.remove(d);
-                } else {
-                    map.put(d, frequency - 1);
+            while(map.size() > k) {
+                    
+                    // slide the window till map.size() <= k
+                    char d = s.charAt(i);
+                    int fre = map.get(d);
+                    
+                    if(fre == 1) {
+                        map.remove(d);
+                    } else {
+                        map.put(d , fre - 1);
+                    }
+                    
+                    i++;
                 }
                 
-                i++;
-            }
             
-            // At this point map.size() <= k count the maxi
             maxi = Math.max(maxi, j - i + 1);
-            
             j++;
-        }
+                
+            }
         
         return maxi;
     }

8a0dbf32500ed77108f3a951cb236bb82d588688
Mohit Sanghai <sanghaimohit100@gmail.com>
2022-04-16 19:20:03 -0400

    Time: 7 ms (76.19%), Space: 42.5 MB (85.65%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,47 +1,45 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-        if (s == null || s.length() == 0) return 0;
+        if(s == null || s.length() == 0) return 0;
         
+        // Longest SubString without repeating Character
         int i = 0;
         int j = 0;
         int maxi = 0;
         
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap();
         
-        while (j < s.length()) {
+        while(j < s.length()) {
             
             char c = s.charAt(j);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            map.put(c, map.getOrDefault(c,0) + 1);
             
-            if (map.size() == j - i + 1) {
-                maxi  = Math.max(maxi, j - i + 1);
+            if(map.size() == j - i + 1) {
+                maxi = Math.max(maxi, j - i + 1);
                 j++;
-            } else if (map.size() < j - i + 1) {
+            } else {
                 
                 while (map.size() < j - i + 1) {
-                   
+                    
                     char d = s.charAt(i);
                     
                     int frequency = map.get(d);
                     
-                    if (frequency == 1) {
-                        
+                    if(frequency == 1) {
                         map.remove(d);
                     } else {
                         map.put(d, frequency - 1);
                     }
                     
-                    i++;   
+                    i++;
                 }
                 
                 j++;
+                
             }
-            
         }
         
-       return maxi; 
-        
-        
+        return maxi;
     }
 }

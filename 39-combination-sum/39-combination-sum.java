83cbf090bad6d9d02f8c020b81d3a0b297670372
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-02 23:12:19 -0400

    Time: 5 ms (60.60%), Space: 45.6 MB (18.78%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    List<List<Integer>> ans = new ArrayList<>();
+    
+    public void helper(int[] candidates, int target, int i, List<Integer> a) {
+        
+        // base case
+        if(i >= candidates.length) {
+            
+            if(target == 0) {
+                ans.add(new ArrayList<>(a)); //IMP
+            }
+            return ;
+        }
+        
+        // add and remove the element in array
+        // Pick up the element
+        if(candidates[i] <= target) {
+            a.add(candidates[i]);
+            helper(candidates, target - candidates[i], i, a);
+            a.remove(new Integer(candidates[i]));
+        }
+        
+        // Not Pickup the element
+        helper(candidates, target, i + 1, a);       
+        
+        
+    }
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<Integer> arr = new ArrayList<>();
+        helper(candidates, target, 0, arr);
+        return ans;
+    }
+}

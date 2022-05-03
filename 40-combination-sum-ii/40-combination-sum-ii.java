99c9b815714cd5a797a7cb646a95b68cea36a958
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-03 01:09:44 -0400

    Time: 4 ms (83.37%), Space: 44.3 MB (28.96%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,23 +1,38 @@
 class Solution {
-    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-        if(target == 0) {
-            ans.add(new ArrayList<>(ds)); 
-            return; 
+    
+    List<List<Integer>> ans = new ArrayList<>(); 
+    
+    private void helper(int[] candidates, int target, int ind, List<Integer> a){        
+        
+        // At some point target will be 0 or less than 0
+        if(target == 0) {            
+            ans.add(new ArrayList<>(a));
+            return ;
         }
         
-        for(int i = ind; i < arr.length;i++) {
-            if(i > ind && arr[i] == arr[i-1]) continue; 
-            if(arr[i]>target) break; 
+        // index can take value from i to end -> we need to use for loop
+        for(int i = ind; i < candidates.length; i++) {            
+            // avaoid duplicates;
+            if(i > ind && candidates[i] == candidates[i - 1]) continue;            
+            // Break the loop - this might happen a lot of times
+            if(candidates[i] > target) break;            
             
-            ds.add(arr[i]); 
-            findCombinations(i+1, arr, target - arr[i], ans, ds); 
-            ds.remove(new Integer(arr[i])); 
-        }
+            a.add(candidates[i]);
+            helper(candidates, target - candidates[i], i + 1, a);
+            a.remove(new Integer(candidates[i]));
+            
+        } 
+    
     }
+    
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(candidates); 
-        findCombinations(0, candidates, target, ans, new ArrayList<>()); 
-        return ans; 
+        
+        List<Integer> arr = new ArrayList<>();
+        
+        Arrays.sort(candidates); // IMP
+        
+        helper(candidates, target, 0, arr);
+        
+        return ans;
     }
 }

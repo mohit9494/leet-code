0bcf4a8c5caf0a8c3b07a027a359791aa94d4ed1
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-20 18:11:15 -0400

    Time: 6 ms (82.32%), Space: 48.3 MB (83.59%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,25 +1,42 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        
-        // Use StairLike Comparison
-        if (matrix.length == 0) return false;
-        
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        int i = 0;
-        int j = n-1;
-        
-        while (i >= 0 && i < m && j>= 0 && j < n) {
-            
-         int val = matrix[i][j];   
-        
-         if (val == target) return true;
-            else if (val < target) i++;
-            else  if (val > target) j--;            
+    private int[][] matrix;
+    private int target;
+
+    private boolean searchRec(int left, int up, int right, int down) {
+        // this submatrix has no height or no width.
+        if (left > right || up > down) {
+            return false;
+        // `target` is already larger than the largest element or smaller
+        // than the smallest element in this submatrix.
+        } else if (target < matrix[up][left] || target > matrix[down][right]) {
+            return false;
         }
-        
-        return false;
-        
+
+        int mid = left + (right-left)/2;
+
+        // Locate `row` such that matrix[row-1][mid] < target < matrix[row][mid]
+        int row = up;
+        while (row <= down && matrix[row][mid] <= target) {
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+            row++;
+        }
+
+        return searchRec(left, row, mid-1, down) || searchRec(mid+1, up, right, row-1);
+    }
+
+    public boolean searchMatrix(int[][] mat, int targ) {
+        // cache input values in object to avoid passing them unnecessarily
+        // to `searchRec`
+        matrix = mat;
+        target = targ;
+
+        // an empty matrix obviously does not contain `target`
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        return searchRec(0, 0, matrix[0].length-1, matrix.length-1);
     }
 }

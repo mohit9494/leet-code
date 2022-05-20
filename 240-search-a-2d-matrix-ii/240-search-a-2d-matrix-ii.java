d16a7cc31caecebaa34efc1182cd77b6edbbf9e2
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-20 18:11:37 -0400

    Time: 9 ms (69.59%), Space: 58.2 MB (20.54%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,48 +1,25 @@
 class Solution {
-    private boolean binarySearch(int[][] matrix, int target, int start, boolean vertical) {
-        int lo = start;
-        int hi = vertical ? matrix[0].length-1 : matrix.length-1;
-
-        while (hi >= lo) {
-            int mid = (lo + hi)/2;
-            if (vertical) { // searching a column
-                if (matrix[start][mid] < target) {
-                    lo = mid + 1;
-                } else if (matrix[start][mid] > target) {
-                    hi = mid - 1;
-                } else {
-                    return true;
-                }
-            } else { // searching a row
-                if (matrix[mid][start] < target) {
-                    lo = mid + 1;
-                } else if (matrix[mid][start] > target) {
-                    hi = mid - 1;
-                } else {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
     public boolean searchMatrix(int[][] matrix, int target) {
-        // an empty matrix obviously does not contain `target`
-        if (matrix == null || matrix.length == 0) {
-            return false;
-        }
-
-        // iterate over matrix diagonals
-        int shorterDim = Math.min(matrix.length, matrix[0].length);
-        for (int i = 0; i < shorterDim; i++) {
-            boolean verticalFound = binarySearch(matrix, target, i, true);
-            boolean horizontalFound = binarySearch(matrix, target, i, false);
-            if (verticalFound || horizontalFound) {
-                return true;
-            }
+        
+        // Use StairLike Comparison
+        if (matrix.length == 0) return false;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int i = 0;
+        int j = n-1;
+        
+        while (i >= 0 && i < m && j>= 0 && j < n) {
+            
+         int val = matrix[i][j];   
+        
+         if (val == target) return true;
+            else if (val < target) i++;
+            else  if (val > target) j--;            
         }
         
-        return false; 
+        return false;
+        
     }
 }

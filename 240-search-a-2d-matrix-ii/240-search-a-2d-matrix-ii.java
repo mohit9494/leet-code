0476217a310446c1db19861c2d4aefe840c96197
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-20 18:11:28 -0400

    Time: 13 ms (29.35%), Space: 57.8 MB (48.59%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,42 +1,48 @@
 class Solution {
-    private int[][] matrix;
-    private int target;
+    private boolean binarySearch(int[][] matrix, int target, int start, boolean vertical) {
+        int lo = start;
+        int hi = vertical ? matrix[0].length-1 : matrix.length-1;
 
-    private boolean searchRec(int left, int up, int right, int down) {
-        // this submatrix has no height or no width.
-        if (left > right || up > down) {
-            return false;
-        // `target` is already larger than the largest element or smaller
-        // than the smallest element in this submatrix.
-        } else if (target < matrix[up][left] || target > matrix[down][right]) {
-            return false;
-        }
-
-        int mid = left + (right-left)/2;
-
-        // Locate `row` such that matrix[row-1][mid] < target < matrix[row][mid]
-        int row = up;
-        while (row <= down && matrix[row][mid] <= target) {
-            if (matrix[row][mid] == target) {
-                return true;
+        while (hi >= lo) {
+            int mid = (lo + hi)/2;
+            if (vertical) { // searching a column
+                if (matrix[start][mid] < target) {
+                    lo = mid + 1;
+                } else if (matrix[start][mid] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            } else { // searching a row
+                if (matrix[mid][start] < target) {
+                    lo = mid + 1;
+                } else if (matrix[mid][start] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
             }
-            row++;
         }
 
-        return searchRec(left, row, mid-1, down) || searchRec(mid+1, up, right, row-1);
+        return false;
     }
 
-    public boolean searchMatrix(int[][] mat, int targ) {
-        // cache input values in object to avoid passing them unnecessarily
-        // to `searchRec`
-        matrix = mat;
-        target = targ;
-
+    public boolean searchMatrix(int[][] matrix, int target) {
         // an empty matrix obviously does not contain `target`
         if (matrix == null || matrix.length == 0) {
             return false;
         }
 
-        return searchRec(0, 0, matrix[0].length-1, matrix.length-1);
+        // iterate over matrix diagonals
+        int shorterDim = Math.min(matrix.length, matrix[0].length);
+        for (int i = 0; i < shorterDim; i++) {
+            boolean verticalFound = binarySearch(matrix, target, i, true);
+            boolean horizontalFound = binarySearch(matrix, target, i, false);
+            if (verticalFound || horizontalFound) {
+                return true;
+            }
+        }
+        
+        return false; 
     }
 }

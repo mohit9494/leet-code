6bc75d8069df1059f99655fd9b727cce3bf48ab4
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-20 17:08:15 -0400

    Time: 0 ms (100.00%), Space: 42.9 MB (42.60%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         
-        if (nums.length == 1) return 0;
+        if(nums.length == 1) return 0;
         
         int left = 0;
         int right = nums.length - 1;
@@ -9,20 +9,21 @@ class Solution {
         while(left <= right) {
             
             int mid = left + (right - left)/2;
-            int leftElement = mid == 0 ? Integer.MIN_VALUE : nums[mid - 1];
-            int rightElement = mid == nums.length - 1 ? Integer.MIN_VALUE : nums[mid + 1];           
             
-            if (nums[mid] > leftElement && nums[mid] > rightElement) {
-                return mid;
-            } else if (leftElement > nums[mid]) {
-                // move to left
+            int val = nums[mid];            
+            int le = mid == 0 ? Integer.MIN_VALUE: nums[mid - 1];
+            int re = mid == nums.length - 1 ? Integer.MIN_VALUE : nums[mid + 1];
+            
+            if (val > le && val > re) return mid;
+            else if (val > re) {
+                // I am on the downword slope
                 right = mid - 1;
             } else {
-                //move to right
                 left = mid + 1;
-            }    
-    
+            }
         }
-       return -1;
+        
+        return -1;
+        
     }
 }

fe3d3ffbd976993703997b84af134de88bd3fda3
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-22 20:04:49 -0400

    Time: 0 ms (100.00%), Space: 42.6 MB (83.94%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -2,33 +2,63 @@ class Solution {
     
     List<List<Integer>> ans = new ArrayList<>();
     
-    private void helper(int[] nums, List<Integer> ds, Set<Integer> set) {
+//     private void helper(int[] nums, List<Integer> ds, Set<Integer> set) {
         
-        if (ds.size() == nums.length) {
-            ans.add(new ArrayList<>(ds));
-            return ;
+//         if (ds.size() == nums.length) {
+//             ans.add(new ArrayList<>(ds));
+//             return ;
+//         }
+        
+//         for(int i = 0; i < nums.length; i++) {
+         
+//             if (!set.contains(nums[i])){
+                
+//             set.add(nums[i]);
+//             ds.add(nums[i]);
+                
+//             helper(nums, ds, set);
+                
+//             set.remove(nums[i]);
+//             ds.remove(ds.size() - 1);
+                
+//             }
+           
+//         }        
+        
+//     }  
+    
+    private void swap(int[] arr, int i, int j) {
+        
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        
+    }
+    
+    private void helper(int[] nums, int index) {
+        
+        if(index >= nums.length) {
+            List temp = new ArrayList<>();
+            
+            for(int i : nums) temp.add(i);
+            
+            ans.add(temp);
+            
+            return;
         }
         
-        for(int i = 0; i < nums.length; i++) {
-         
-            if (!set.contains(nums[i])){
-                
-            set.add(nums[i]);
-            ds.add(nums[i]);
-                
-            helper(nums, ds, set);
-                
-            set.remove(nums[i]);
-            ds.remove(ds.size() - 1);
-                
-            }
-           
-        }        
+        // swapping
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, index, i);
+            helper(nums, index + 1);
+            swap(nums, index, i);
+            
+        }
         
-    }    
+    }
     public List<List<Integer>> permute(int[] nums) {
         
-        helper(nums, new ArrayList<>(), new HashSet<>());
+        helper(nums, 0);
         
         return ans;
         

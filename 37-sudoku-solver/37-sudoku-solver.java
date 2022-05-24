41eebd6f8b8e474415f22c5987ef318c8e4b47d2
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-24 19:34:22 -0400

    Time: 21 ms (35.26%), Space: 42.1 MB (31.60%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,51 +1,66 @@
 class Solution {
     
-    private boolean isValid(char[][] board, char c, int row, int col) {
+    private boolean isValid(char[][] board, int row, int col, char c) {
         
-        for(int i = 0; i < 9 ; i++) {
-            
-            if(board[row][i] == c) return false;
+        for (int i = 0; i < 9; i++) {
+          
+            if(board[row][i]  == c) return false;
             
             if(board[i][col] == c) return false;
             
-            if(board[ 3 * (row/3) + i/3][3 * (col/3) + i%3] == c) return false;
+            int rowBlock = 3 * (row/3);
+            int colBlock = 3 * (col/3);
+            
+            int rowPos = rowBlock + (i/3);
+            int colPos = colBlock + (i % 3);
+            
+            if(board[rowPos][colPos] == c) return false;
         }
         
-        return true;
+     return true; 
+        
     }
     
-    private boolean solve(char[][] board) {
+    public boolean solve(char[][] board) {
         
-    // check if any empty spot is available
-        for(int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9 ; j++) {
+      // Find the empty spot
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 
-                if(board[i][j] == '.') {
+                if (board[i][j] == '.') {
                     
+                    // Found the empty spot
+                    // Check for all the numbers from 1 to 9 
                     for (char c = '1'; c <= '9'; c++) {
                         
-                        if (isValid(board, c, i, j)){
-                          
-                          board[i][j] = c;
-                          
-                          if (solve(board)){
-                              return true;
-                          } else {
-                              board[i][j] = '.';
-                          }
+                        // If its a valid character
+                        if (isValid(board, i, j, c)) {
                             
-                        }
-                    }
+                            board[i][j] = c;
+                            
+                            boolean flag = solve(board);
+                            if (flag) return true; // Got the only answer needed
+                            else board[i][j] = '.'; // else restore                            
+                            
+                        }                       
+                        
+                    }                    
+                    
+                    // At this point the spot is '.' but not able to fill any number between 1 to 9
+                    // return false as this combination is not gonnaa work at all
                     return false;
                 }
                 
             }
         }
+        // There is no spot to fill -> Board is solved
         return true;
     }
     
-    
     public void solveSudoku(char[][] board) {
+        
+        // Board full of chars
+        // . for empty
         
         solve(board);
         

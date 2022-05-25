08b69e74b7482cf58474c8da32c8a545acbc597a
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-05-25 00:41:38 -0400

    Time: 10 ms (82.60%), Space: 54.7 MB (88.18%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,45 +1,44 @@
 class Solution {
     
-    
     List<List<String>> ans = new ArrayList<>();
     
-    private boolean isPalindrome(String s, int start, int end) {
+    private boolean isPalindrome(String s, int i, int j) {
         
-        while (start <= end) {
-            if(s.charAt(start++) != s.charAt(end--)){
-                return false;
-            }
+        while (i <= j) {
+        
+            if (s.charAt(i++) != s.charAt(j--)) return false;
+            
         }
+        
         return true;
     }
     
-    private void getPartition(String s, int index, List<String> ds) {
+    private void partitionString(String s, int index, List<String> ds) {
         
-        if(index >= s.length()) {
-            
+        if (index >= s.length()) {
             ans.add(new ArrayList<>(ds));
             return;
         }
         
         for (int i = index; i < s.length(); i++) {
-            
-            if(isPalindrome(s, index, i)) {
+         
+            // If its a palindrome
+            if (isPalindrome(s, index, i)){
                 
                 ds.add(s.substring(index, i + 1));
-                
-                getPartition(s, i + 1, ds);
+                       
+                partitionString(s, i + 1, ds);
                 
                 ds.remove(ds.size() - 1);
-            }
+            }            
             
-        }
-        
+        }       
         
     }
     
     public List<List<String>> partition(String s) {
         
-        getPartition(s, 0, new ArrayList<>());
+        partitionString(s, 0, new ArrayList<>()); 
         
         return ans;
     }

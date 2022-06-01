bd8fab0441db172f61f6220355e16286431f0fa0
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-01 01:50:38 -0400

    Time: 4 ms (63.60%), Space: 41.9 MB (74.66%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,21 +1,22 @@
 class Solution {
     public int longestPalindrome(String s) {
         
-        int[] a = new int[256];
         int count = 0;
-        boolean flag = true;
         
-        for (int i = 0; i < s.length(); i++) {
-            a[s.charAt(i)]++;
+        Set<Character> set  = new HashSet();
+        
+        for (char c : s.toCharArray()) {
+            
+            if (set.contains(c)) {
+                count += 2 ;
+                set.remove(c);
+            } else {
+                set.add(c);
+            }
+            
         }
         
-        // Get count by dividing and multiply with 2 -> ccc
-        for (int i : a) {
-           count = count + ((i/2) * 2);
-        }
-        
-        // Handling odd occurances
-        return count == s.length() ? count : count + 1;
-        
+      if(set.isEmpty()) return count;
+        else return count + 1;
     }
 }

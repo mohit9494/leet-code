3b95a968a68a4178a7427ceee44fe4ba8a46b0ce
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-04 02:14:32 -0400

    Time: 7 ms (52.74%), Space: 48.5 MB (49.80%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -3,32 +3,28 @@ class Solution {
         
         if (coins == null || coins.length == 0) return 0;
         
+        // return total number of ways
         int r = coins.length;
         int c = amount;
         
         int[][] dp = new int[r + 1][c + 1];
-        
-        // Initialize the dp
-        // Keep in mind that you are counting total number of Ways not coins -> 
-        // sum the ways at node junction
-                
+                                                
+        // Initialize first dummy row in dp
         dp[0][0] = 1;
-                
+        
         for (int i = 1; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
-             
-                // If the amount < than the coin denomination
-                if (j < coins[i - 1]) {
+                
+                // If amount  < denomination in coins array
+                if(j < coins[i - 1]) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
                     dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-                }               
+                }
                 
             }
         }
-        
-        
-
-        return dp[r][c];
+                                                
+        return dp[r][c];                                        
  }
 }

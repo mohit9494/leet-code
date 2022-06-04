54a7e6161909bed7cd522d3107cd29133253d36f
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-04 01:47:33 -0400

    Time: 46 ms (26.61%), Space: 57.4 MB (14.80%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,42 +1,35 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        
-        if (coins.length == 0 || amount == 0) return 0;
-        
-        // dp array
-        // row - coin denomination
-        // cols - amount
+      
+        if(coins == null || coins.length == 0) return 0;
         
         int r = coins.length;
         int c = amount;
         
-        // +1 to handle the 0 values
+        // we are taking dummy 0th row
         int[][] dp = new int[r + 1][c + 1];
         
-        // first row initialization
+        // Initialize dummy row
+        dp[0][0] = 0;
         for(int j = 1; j < dp[0].length; j++) {
-            // Just put any impossible value
-            dp[0][j] = amount + 1; 
+            dp[0][j] = amount + 1; // Impossible value = Infinity 
         }
         
-        for (int i = 1; i < dp.length; i++){
-            for (int j = 1; j < dp[0].length; j++) {
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
                 
-                // here coins[i - 1] => why -1 ?
-                // because we added 1 dummy row as 0 first
-                // j => amount
-                if (j < coins[i - 1]) {
-                 dp[i][j] = dp[i - 1][j];   
+                // If amount < coin denomination -> Do not select -> Take previous
+                if(j < coins[i - 1])// why i-1 ? -> as there is dummy row
+                {
+                    dp[i][j] = dp[i - 1][j];
                 } else {
+                    // Min between pick and non-pick
                     dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
                 }
                 
-                
             }
         }
-          
-         int ans = dp[r][c]; 
         
-        return ans >= amount + 1 ? -1 : ans;
+        return dp[r][c] >= amount + 1 ? -1 : dp[r][c];
     }
 }

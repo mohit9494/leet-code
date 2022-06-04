6607535eaa33fee05acaef94f5d232abb85abc75
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-04 16:57:46 -0400

    Time: 0 ms (100.00%), Space: 41 MB (68.33%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -10,17 +10,19 @@ class Solution {
         
         // Initialize the dp array
         // we need (i - 2) amount so we need to initialize first 2 places
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int prev = nums[0];
+        int curr = Math.max(nums[0], nums[1]);
         
         // start from 2
         for (int i = 2; i < n; i++) {
             
+            int temp = curr;
             // Take max from not choose and choose
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+            curr = Math.max(curr, nums[i] + prev);
             
+            prev = temp;
         }
         
-        return dp[n - 1];
+        return curr;
     }
 }

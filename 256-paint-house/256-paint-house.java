f7c7fd02fb6260845be3618351f555337dd13c5f
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-04 03:45:51 -0400

    Time: 1 ms (87.08%), Space: 43.5 MB (41.07%) - LeetHub

---
+++ 256-paint-house/256-paint-house.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minCost(int[][] costs) {
+        
+        if(costs == null || costs.length == 0) return 0;
+        
+        int n = costs.length;
+        
+        int[][] dp = new int[n][3];
+        
+        // Initialize dp array last roq -> we are going bottom up
+        dp[n - 1][0] = costs[n-1][0];
+        dp[n - 1][1] = costs[n-1][1];
+        dp[n - 1][2] = costs[n-1][2];
+        
+        // start from the second last row
+        for(int i = n-2; i >= 0; i--) {            
+               
+         dp[i][0] = costs[i][0] + Math.min(dp[i + 1][1], dp[i + 1][2]);
+         dp[i][1] = costs[i][1] + Math.min(dp[i + 1][0], dp[i + 1][2]);
+         dp[i][2] = costs[i][2] + Math.min(dp[i + 1][0], dp[i + 1][1]);
+            
+        }
+        
+        // return the result from first row
+        return Math.min(dp[0][0], Math.min(dp[0][1], dp[0][2]));
+    }
+}

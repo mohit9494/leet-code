38fe0ff5ed1962aa5a06ff33ac589ffe2f99a976
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-05 06:54:39 -0400

    Time: 36 ms (18.12%), Space: 58.9 MB (20.11%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,51 +1,32 @@
 class Solution {
     
-    List<List<String>> ans = new ArrayList<>();
-    
-    Map<String, List<String>> map = new HashMap<>();
-    
-    private String generateKey(String s){
-        
-        char[] ca = new char[26];
-        
-        for (char c : s.toCharArray()) ca[c - 'a']++ ;
-        
-        return String.valueOf(ca);
-        
-    }
-    
-    private String sortString(String s) {
-        
-        char[] ca = s.toCharArray();
-        Arrays.sort(ca);        
-        return String.valueOf(ca);       
-        
-    }
+    private String generateKey(String s) {
+        char[] c = s.toCharArray();
+         Arrays.sort(c);
+        return String.valueOf(c);
+}
     
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        if (strs.length == 0) return ans;
+        List<List<String>> ans = new ArrayList<>();
         
-        // Generate Map
-        for (String s : strs) {
+        Map<String, List<String>> map = new HashMap();
+        
+        for(String s : strs) {
             
-         // sort each String and use it as a key
-         //   String key = sortString(s);  
-           
-            // Generate Key
             String key = generateKey(s);
-            
+           
             if (!map.containsKey(key)) map.put(key, new ArrayList<>());
             
             map.get(key).add(s);
-            
-        }
-            
-            // IMP
-            for (List<String> lst : map.values()) ans.add(lst); 
-            
-             return ans;            
-            
-        }     
-     
+           
+         }
+        
+        System.out.println(map);
+        
+        // Get result from map
+        for(List<String> ls : map.values()) ans.add(ls);
+        
+        return ans;
     }
+}

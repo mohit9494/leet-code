0a61d2d1560c2dbb4ad964b7a40dc7460901307d
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-05 07:30:05 -0400

    Time: 1 ms (93.69%), Space: 41.6 MB (61.01%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,31 +1,27 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         
-        // Length of pattern should match array of s
-        if (pattern.length() != s.split(" ").length) return false;
+        String[] arr = s.split(" ");
         
-        // Map of char with String and String with Char -> Full Match
+        if (pattern.length() != arr.length) return false;
+        
         Map<Character, String> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>();     
         
-        String[] words = s.split(" ");
         
         for (int i = 0; i < pattern.length(); i++) {
             
             char c = pattern.charAt(i);
-            String word = words[i];
+            String word = arr[i];
             
-            if (map.containsKey(c)) {
-                String temp = map.get(c);
-                if (!temp.equals(word)) return false;
+            if(map.containsKey(c)) {
+                if(!map.get(c).equals(word)) return false;
             } else {
                 
-                // check if value is present in set
-                if (set.contains(word)) return false; // double mapping
+                if(set.contains(word)) return false;
                 
                 map.put(c, word);
                 set.add(word);
-                
             }
             
         }

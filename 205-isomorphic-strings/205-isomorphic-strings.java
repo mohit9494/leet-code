976e6a08c10f4df9f90b2f2c812eb9b8ab18cbc7
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-05 07:14:25 -0400

    Time: 5 ms (88.97%), Space: 42.3 MB (78.06%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,23 +1,21 @@
 class Solution {
-    
-    // ASCII characters -> array of size 256
-    int[] c1 = new int[256];
-    int[] c2 = new int[256];
-    
     public boolean isIsomorphic(String s, String t) {
         
-        for (int i = 0; i < s.length(); i++) {
-                 
+        if (s.length() != t.length()) return false;
+        
+        int[] sChar = new int[256];
+        int[] tChar = new int[256];
+        
+        for(int i = 0; i < s.length(); i++) {
+            
             char a = s.charAt(i);
             char b = t.charAt(i);
             
-            if (c1[a] != c2 [b]) return false;
+            if (sChar[a] != tChar[b]) return false;
             
-            c1[a] = i + 1;
-            c2[b] = i+1;
-            
+            sChar[a] = i + 1;
+            tChar[b] = i + 1;
         }
-        
         return true;
     }
 }

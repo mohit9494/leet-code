755a43d84550dfb43c33de2c4b011e0916509565
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-09 03:48:33 -0400

    Time: 1 ms (93.91%), Space: 53.9 MB (59.48%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,8 +1,15 @@
  public class Solution {
  public void setZeroes(int[][] matrix) {
+     // check initial condition
      if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+     
+     // Get rows and cols 
      int m = matrix.length, n = matrix[0].length;
+     
+     // set flag for first row and cols
      boolean row = false, col = false;
+     
+     // set up the marker
      for (int i = 0; i < m; i++)
          for (int j = 0; j < n; j++){
              if (matrix[i][j] == 0) {
@@ -12,18 +19,24 @@
                  if (j == 0) col = true;
              }
          }
+     
+     // check all row
      for (int i = 1; i < m; i++){
          if (matrix[i][0] == 0){
              for (int j = 1; j < n;j++)
                  matrix[i][j] = 0;
          }
      }
+     
+     // check all cols
      for (int j = 1; j < n; j++){
          if (matrix[0][j] == 0){
              for (int i = 1; i < m; i++)
                  matrix[i][j] = 0;
          }
      }
+     
+     // Change First row and col
      if (row){
          for (int j = 0; j < n; j++)
              matrix[0][j] = 0;
@@ -32,5 +45,6 @@
          for(int i = 0; i < m; i++)
              matrix[i][0] = 0;
      }
+     
  }
  }

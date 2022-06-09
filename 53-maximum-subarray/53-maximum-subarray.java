a723d4306c63523d39cc1c6c89faba6135e7eabb
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-09 01:06:45 -0400

    Time: 2 ms (59.21%), Space: 82.2 MB (5.52%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,21 +1,22 @@
 class Solution {
-    
     public int maxSubArray(int[] nums) {
+        
         
         int maxi = Integer.MIN_VALUE;
         int sum = 0;
         
-        for (int i : nums) {
-          
-            sum += i;
+        for (int i = 0; i < nums.length; i++) {
+            
+            sum = sum + nums[i];
             
             maxi = Math.max(maxi, sum);
             
-            // This is IMP ..REMOVIING EFFECTS of -ve numbers
-            if (sum < 0 ) sum = 0;
+            // Kadane's
+            if(sum < 0) sum = 0;
             
         }
         
         return maxi;
+        
     }
 }

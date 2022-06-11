409d9bcbe05bd564a1218bb542bda181fa13ec02
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-11 02:14:35 -0400

    Time: 1 ms (74.95%), Space: 44.5 MB (58.90%) - LeetHub

---
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int k = 2;
+        
+        int i = 1, j = 1, count = 1;
+        
+        while (j < nums.length) {
+            
+          if (nums[j] == nums[j - 1]) {
+              count++;
+          } else {
+              count = 1;
+          }
+            
+            // Check count
+            if (count <= k ) {
+                // move i with j
+                nums[i++] = nums[j];
+            }
+            
+            j++;
+        }
+        
+        return i;
+    }
+}

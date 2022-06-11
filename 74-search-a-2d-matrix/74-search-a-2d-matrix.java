527db5b898c42d0c4c14b4b9ad32f04b8ce53935
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-11 02:47:24 -0400

    Time: 1 ms (30.10%), Space: 42.1 MB (81.51%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,31 +1,23 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        // Stair like comparison
-        if (matrix.length == 0) return false;
+        // This is Pure Binary Search
+        int m = matrix.length;
+        int n = matrix[0].length;
         
-        int m  = matrix.length; //row
-        int n = matrix[0].length; // col
-           
-        int left = 0;
-        int right = m * n - 1;
+        int lo = 0;
+        int hi = m * n - 1;
         
-        while(left <= right) {
+        while (lo <= hi) {
             
-            int mid = left + (right - left)/2;
+            int mid = lo + (hi - lo)/2;
             
-            //calculate row and col from mid
-            int i = mid / n;
-            int j = mid % n;
+            int element = matrix[mid / n][mid % n];
             
-            int val = matrix[i][j];
-            
-            if (val == target) return true;
-            else if (val < target) left = mid + 1;
-            else right = mid - 1;
-            
+            if (element == target) return true;         
+            else if (element < target) lo++;
+            else hi--;
         }
-        
-        return false;
+      return false;  
     }
 }

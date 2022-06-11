2e4f5871660896e049bd6dc6d9c8de9989942d57
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-11 03:01:15 -0400

    Time: 6 ms (83.65%), Space: 48.5 MB (80.32%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -8,7 +8,7 @@ class Solution {
         int i = 0, j = n - 1;
         int a = m - 1, b = 0;
         
-        while (i < m && j >= 0 & a >= 0 && b < n) {
+        while (i < m && j >= 0 && a >= 0 && b < n) {
             
             int num = matrix[i][j];
             int num1 = matrix[a][b];

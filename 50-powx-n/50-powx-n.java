ec95b57f5fffc40c6e36c4d662e5796a11a2b6ea
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-11 23:26:09 -0400

    Time: 2 ms (15.31%), Space: 42.8 MB (50.50%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,18 +1,33 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        double ans = 1;
-        double current_product = x;
-        for (long i = N; i > 0; i /= 2) {
-            if ((i % 2) == 1) {
-                ans = ans * current_product;
+        
+      long N = n;
+        
+      if (N == 0) return 1.0;
+        
+     if (N < 0) {
+         N = -N;
+         x = 1 / x;
+     }
+        
+        double ans = 1.0;
+        
+        while (N > 0) {
+            
+            // If n is even
+            if (N % 2 == 0) {
+                
+                x = x * x;
+                N =  N / 2;
+                
+            } else  {
+                
+             ans = ans * x;
+             N = N - 1;
+                
             }
-            current_product = current_product * current_product;
         }
+       
         return ans;
     }
-};
+}

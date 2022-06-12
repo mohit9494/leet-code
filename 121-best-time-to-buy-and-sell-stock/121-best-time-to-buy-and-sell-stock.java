68b7695817fd24acc688895f6498ae188a4dd118
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-11 22:01:28 -0400

    Time: 2 ms (88.68%), Space: 58.7 MB (94.40%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
+        // This is Max differece between two Values
+        // Kadane's algo
+        
         int maxi = 0;
+        int sum = 0;
         
-        if(prices.length <= 0) return maxi;
-        
-        int min = Integer.MAX_VALUE;
-        int j = 0;
-        
-        while (j < prices.length) {
-
-         if (prices[j] < min) min = prices[j];
+        for (int i = 0; i < prices.length - 1; i++) {
             
-        maxi = Math.max(maxi, prices[j] - min);
+            sum += prices[i + 1] - prices[i];
             
-         j++;   
+            maxi = Math.max(maxi, sum);
             
-        }        
+            // If sum = -ve then its useless
+            if(sum < 0) sum = 0;
+            
+        }
         
         return maxi;
     }

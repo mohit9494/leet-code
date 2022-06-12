63690e3b974b2c5297d9cb424ae94121c05b3b53
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-12 03:52:56 -0400

    Time: 2 ms (81.03%), Space: 57.9 MB (40.20%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         
-        // Generate left array
-        int[] lp = new int[nums.length];
-        lp[0] = 1;        
-        for (int i = 1; i < nums.length; i++) lp[i] = lp[i - 1] * nums[i - 1];
-        
-        // Generate right array
-        int[] rp = new int[nums.length];
-        rp[nums.length - 1] = 1;
-        
-        for (int i = nums.length - 2; i >= 0; i--) rp[i] = rp[i + 1] * nums[i + 1];
-        
+        // Generate left array and use it to store ans
         int[] ans = new int[nums.length];
+        ans[0] = 1;
+        for (int i = 1; i < nums.length; i++) ans[i] = ans[i - 1] * nums[i - 1];
         
-        for (int i = 0; i < nums.length; i++) ans[i] = lp[i] * rp[i];
+        // Generate Right product Dianamically
+        int r = 1;
         
-        return ans;
-        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] = ans[i] * r;
+            r = r * nums[i];
+        }
+      
+        return ans;  
     }
 }

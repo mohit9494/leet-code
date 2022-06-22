909e677e1e20bfd8d7fb8b646b3431230f630493
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-22 03:53:43 -0400

    Time: 1 ms (88.29%), Space: 44.5 MB (10.45%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -5,11 +5,11 @@ class Solution {
         // top of floor is beyond array
         // u only pay till step i then from i jump 1 or 2
         int n = cost.length;
-        int[] dp = new int[n + 1];
+        // int[] dp = new int[n + 1];
         
         // Initialize dp array
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        int a = cost[0];
+        int b = cost[1];
         
         // here we need to reach n
         // to get there we need to pay toll till either n - 1 or n - 2 :: from there we can jump 1 or 2 steps for free
@@ -17,11 +17,13 @@ class Solution {
         //now get the mimimum to reach n - 1 or n-2;
         
         for (int i = 2; i < n; i++) {
-            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+            int c = cost[i] + Math.min(a, b);
+            a = b;
+            b = c;
         }
         
         // calculating the naswer for nth destination
-        return Math.min(dp[n - 1], dp[n - 2]);
+        return Math.min(a, b);
         
     }
 }

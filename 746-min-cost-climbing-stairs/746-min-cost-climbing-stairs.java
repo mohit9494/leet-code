e87d495367d422ab3e3de4529c7c271f865466f2
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-22 03:51:30 -0400

    Time: 1 ms (88.29%), Space: 43 MB (78.45%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,17 +1,5 @@
 class Solution {
     
-    private int helper(int[] cost, int n, int[] dp) {
-        
-    if (n < 0) return 0;
-      // we can start from either index 0 or 1
-    if (n == 0 || n == 1) return cost[n];
-     
-    if (dp[n] != -1) return dp[n];    
-        
-    return dp[n] = cost[n] + Math.min(helper(cost, n - 1, dp), helper(cost, n - 2, dp));
-        
-    }
-    
     public int minCostClimbingStairs(int[] cost) {
         
         // top of floor is beyond array
@@ -19,13 +7,21 @@ class Solution {
         int n = cost.length;
         int[] dp = new int[n + 1];
         
-        Arrays.fill(dp, -1);
+        // Initialize dp array
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
         // here we need to reach n
         // to get there we need to pay toll till either n - 1 or n - 2 :: from there we can jump 1 or 2 steps for free
         // and we will reach n
         //now get the mimimum to reach n - 1 or n-2;
         
-        return Math.min(helper(cost, n - 1, dp), helper(cost, n - 2, dp));
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+        
+        // calculating the naswer for nth destination
+        return Math.min(dp[n - 1], dp[n - 2]);
         
     }
 }

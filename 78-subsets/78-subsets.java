ac8b3cf147715121993d68b12dd19cdae7274458
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-06-29 02:36:05 -0400

    Time: 1 ms (88.24%), Space: 43.1 MB (65.38%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,28 +1,29 @@
 class Solution {
     
-    List<List<Integer>> ans = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
     
     private void helper(int[] nums, int index, List<Integer> ds) {
         
         if (index == nums.length) {
-            ans.add(new ArrayList<>(ds));
+            result.add(new ArrayList<>(ds));
             return;
         }
         
-        // pick and non-pick
+        // choose
         ds.add(nums[index]);
         helper(nums, index + 1, ds);
         ds.remove(ds.size() - 1);
         
-        // Non Pick
+        // Not choose
         helper(nums, index + 1, ds);
-        
     }
     
     public List<List<Integer>> subsets(int[] nums) {
         
+        if (nums == null || nums.length == 0) return result;
+        
         helper(nums, 0, new ArrayList<>());
         
-        return ans;
+        return result;
     }
 }

04dfa56185aedac3fac41e7fedd49e1bce1b9f11
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-04 18:38:19 -0400

    Time: 6 ms (59.14%), Space: 45.7 MB (8.09%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,53 +1,15 @@
 class Solution {
-    
-    private void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-    
-    private int partition(int[] a, int lo, int hi) {
-        
-        int i = lo;
-        int j = hi + 1;
-        
-        while (true) {
-        while (a[++i] < a[lo]) 
-            if (i == hi) break;
-        
-        while (a[lo] < a[--j])
-            if(j == lo) break;
-        
-        if (i >= j) break;
-        
-        swap(a, i, j);
-            
-        }
-        
-      swap(a, lo, j);
-      
-      return j;
-        
-    }
-    
     public int findKthLargest(int[] nums, int k) {
         
-    int lo = 0;
-    int hi = nums.length - 1;
-    
-    k = nums.length - k;
+        // Priority Queue
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-    while (hi > lo) {
-    
-   int j = partition(nums, lo, hi);
-                     
-    if (j == k) return nums[k];
-    else if (j < k) lo = j + 1;
-    else hi = j - 1;
-                     
-    }
+        for (int num : nums) {
+            pq.add(num);
+            
+            if (pq.size() > k) pq.poll();
+        }
         
-    return nums[k];
-        
+        return pq.poll();
     }
 }

e3165b717628d2ed83721e4d1f73292e6e6c2426
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-09 01:40:49 -0400

    Time: 0 ms (100.00%), Space: 48.1 MB (38.71%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -1,31 +1,20 @@
 class Solution {
-    
-    public int ceilIndex(char[] letters, char target) {
-        
-        int left = 0;
-        int right  = letters.length - 1;
-        
-        while (left <= right) {
-            
-            int mid = left + (right - left)/2;
-            
-            if (letters[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-            
-        }
-        
-        return left;
-    }
-    
     public char nextGreatestLetter(char[] letters, char target) {
         
-        int i = ceilIndex(letters, target);
+        int left = 0;
+        int right = letters.length - 1;
+
+        if(target >= letters[right]) return letters[0];
+
+        while(left <= right) {
+
+            int mid = left + (right - left)/2;
+            if (letters[mid] <= target) left = mid + 1;
+            else right = mid - 1;
+
+        }
+
+        return letters[left];
         
-        if (i == letters.length) return letters[0];
-        
-        return letters[i];
     }
 }

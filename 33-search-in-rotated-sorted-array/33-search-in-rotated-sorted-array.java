e864b7e57c68c8017846af6a0bbc12ad4a7a916f
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-08 20:43:17 -0400

    Time: 1 ms (73.63%), Space: 42.7 MB (46.72%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,14 +1,36 @@
 class Solution {
     
-    public int searchHelper(int[] nums, int target, int left, int right) {
+    private int getPivot(int[] nums) {
         
-        while(left <= right) {
-          
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while(left < right) {
+            
+            int mid=  left + (right - left)/2;
+            
+            if(nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+            
+            
+        }
+        
+        return left;
+        
+    }
+    
+    public int bs(int[] nums, int left, int right, int t){
+        
+        while (left <= right) {
+            
             int mid = left + (right - left)/2;
             
-            if (nums[mid] == target) return mid;
-            else if(nums[mid] < target) left = mid + 1;
-            else right = mid - 1; 
+            if(nums[mid] == t) return mid;
+            else if (nums[mid]< t) left = mid + 1;
+            else right = mid - 1;
             
         }
         
@@ -17,29 +39,14 @@ class Solution {
     
     public int search(int[] nums, int target) {
         
-        // Find the pivot element Index
-        int left = 0;
-        int right = nums.length - 1;
+      // get the pivot index;
+      int pivot = getPivot(nums);
+      if(nums[pivot] == target) return pivot; 
         
-        while(left < right) {
-            
-            int mid = left + (right - left)/2;
-            
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        
-        // Got the pivot index
-        int rot = left;
-        
-        int i = searchHelper(nums,target, 0, rot - 1);
-        int j = searchHelper(nums, target, rot, nums.length - 1);
-        
-        if (i == -1 && j == -1) return -1;
-        else if (i == -1) return j;
-        else return i;
+     int a = bs(nums, 0, pivot - 1, target);
+     int b = bs(nums, pivot + 1, nums.length - 1, target);
+     
+    return Math.max(a, b);
+    
     }
 }

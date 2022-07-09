3a44b330ba66007d496010ef9b6c003fbee74fcd
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-09 01:01:26 -0400

    Time: 0 ms (100.00%), Space: 43.8 MB (28.22%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -4,16 +4,17 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
         
-        while(left <= right){
+        if (target > nums[right]) return right + 1;
+        
+        while (left < right) {
             
             int mid = left + (right - left)/2;
             
-            if(nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
-            
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
         }
         
-        return left;
+        // single one left ==> 
+       return left;
     }
 }

128df1efd00be48e56517600b41b028b53319154
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-09 01:03:38 -0400

    Time: 0 ms (100.00%), Space: 44 MB (17.00%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -4,6 +4,7 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
         
+        // target might be greater than the right element
         if (target > nums[right]) return right + 1;
         
         while (left < right) {

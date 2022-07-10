b850ce2f432c46b58e20089a6363a79d225ad73c
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-09 20:14:30 -0400

    Time: 274 ms (5.11%), Space: 137 MB (5.05%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -1,16 +1,41 @@
 class Solution {
+    
+    private List<Integer> countSort(int[] arr) {
+        
+        int size = 2*(int)(Math.pow(10,6));
+        size++;
+        
+        int[] count = new int[size];
+        int shift = (int)Math.pow(10,6);
+        
+        for (int i : arr) {
+            count[i + shift] = 1;
+        }
+        
+        List<Integer> sorted = new ArrayList<>();
+        
+        for(int i = 0; i< count.length; i++) {
+            if(count[i] == 1) sorted.add(i - shift);
+        }
+        
+        return sorted;
+    }
+    
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         
         List<List<Integer>> result = new ArrayList<>();
         
+        // sort the array using counting sort
+        List<Integer> sorted = countSort(arr);
+        
         // sort the array
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
         int min = Integer.MAX_VALUE;
         
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < sorted.size() - 1; i++) {
             
-            int a = arr[i];
-            int b = arr[i + 1];
+            int a = sorted.get(i);
+            int b = sorted.get(i + 1);
             int diff = Math.abs(b - a);
             
             if (diff < min) {

bf37bf3674202f2f103323d9f37f3d04b192cd0a
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-11 05:17:53 -0400

    Time: 35 ms (65.01%), Space: 50.7 MB (99.05%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.java
+++ 525-contiguous-array/525-contiguous-array.java
@@ -1,41 +1,28 @@
 class Solution {
     public int findMaxLength(int[] nums) {
         
-        int ans = 0;
-        
-        if (nums == null || nums.length == 0) return ans;
-        
-        int sum = 0;
         int k = 0;
+        int sum = 0;
+        int maxi = 0;
         
         Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
-        
-            int element = nums[i];
             
-            if (element == 0) {
-                sum += -1;
+            if (nums[i] == 0) sum--;
+            else sum++;
+            
+            if (sum == k) {
+                maxi = i + 1;
             } else {
-                sum += 1;
+                if (map.containsKey(sum - k)) {
+                    maxi = Math.max(maxi, i - map.get(sum - k));
+                }
             }
             
-            // Now process
-            if(sum == k) {
-                ans = i + 1;
-            } else {
-            
-            if (map.containsKey(sum - k)) {
-                ans = Math.max(ans, i - map.get(sum - k));
-            }
-            
-            // put the first instance only
             map.putIfAbsent(sum, i);
-                
-            }
         }
         
-        return ans;
-        
+        return maxi;
     }
 }

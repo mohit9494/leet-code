04728a33bf9f8696b6899aac64945b4d0aaeee44
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-16 21:49:37 -0400

    Time: 6 ms (94.46%), Space: 42.1 MB (96.03%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int calculate(String s) {
+        
+        // very simple solution
+        int result = 0;
+        int num = 0;
+        int sign = 1;
+        
+        // we will use 1 and -1 as a sign for + and  -
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+         char c = s.charAt(i);
+        
+         if (Character.isDigit(c)) {
+             num = num * 10 + (c - '0');
+         } else if (c == '+'){
+             result += sign * num;
+             num = 0;
+             sign = 1;
+         } else if (c == '-') {
+             result += sign * num;
+             num = 0;
+             sign = -1;
+         } else if (c == '(') {
+            // push all the result calculated till now to stack
+            // push the sign as well - will be used later
+            st.add(result);
+            st.add(sign);
+            result = 0;
+            sign = 1;
+         } else if (c == ')') {
+             result += sign * num;
+             
+             // Get the sign 
+             result *= st.pop();
+             // total result
+             result += st.pop();
+             num = 0;
+         }
+            
+            
+        }
+        
+        // for last num
+        if (num != 0) result += sign * num;
+        
+        return result;
+    }
+}

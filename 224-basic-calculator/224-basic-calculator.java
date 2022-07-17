42d0c4732a56d3699ab7b7ed5b056c00ba0d23ab
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-17 03:27:22 -0400

    Time: 12 ms (65.55%), Space: 44.4 MB (48.22%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,51 +1,52 @@
 class Solution {
-    public int calculate(String s) {
+    public int calculate(String s) {        
+        // since this has () -> we need to use stack       
+        s = s.trim();
         
-        // very simple solution
         int result = 0;
         int num = 0;
         int sign = 1;
         
-        // we will use 1 and -1 as a sign for + and  -
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> st = new Stack<>();        
         
         for (int i = 0; i < s.length(); i++) {
             
-         char c = s.charAt(i);
-        
-         if (Character.isDigit(c)) {
-             num = num * 10 + (c - '0');
-         } else if (c == '+'){
-             result += sign * num;
-             num = 0;
-             sign = 1;
-         } else if (c == '-') {
-             result += sign * num;
-             num = 0;
-             sign = -1;
-         } else if (c == '(') {
-            // push all the result calculated till now to stack
-            // push the sign as well - will be used later
-            st.add(result);
-            st.add(sign);
-            result = 0;
-            sign = 1;
-         } else if (c == ')') {
-             result += sign * num;
-             
-             // Get the sign 
-             result *= st.pop();
-             // total result
-             result += st.pop();
-             num = 0;
-         }
+            char c = s.charAt(i);
             
-            
+            if (Character.isDigit(c)) {
+               num = num * 10 + c - '0'; 
+            } else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                // store result and sign into the stack -> we will use it later
+                st.add(result);
+                st.add(sign);
+                
+                // reset them
+                result = 0;
+                sign = 1;
+                num = 0;
+            } else if (c == ')') {
+                
+                result += sign * num;
+                num = 0;
+                sign = 1;
+                
+                // get the previous result and sign from the stack and merge it
+                result *= st.pop();
+                result += st.pop();
+            }
+         
+            }
+            // add final digit if there
+            result += sign * num;
+             return result;
         }
         
-        // for last num
-        result += sign * num;
-        
-        return result;
-    }
 }

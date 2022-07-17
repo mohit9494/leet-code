4f1fe2db66a01ce8098ad8503fea561518e2b813
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-17 04:03:26 -0400

    Time: 31 ms (29.29%), Space: 44.9 MB (53.03%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -1,47 +1,49 @@
 class Solution {
     public int calculate(String s) {
         
-        s = s.trim();
-        
+        // get the number -- evaluate and dump it in stack
         Stack<Integer> st = new Stack<>();
-        char lastsign = '+';
+        char sign = '+';
         
         for (int i = 0; i < s.length(); i++) {
             
-            char c = s.charAt(i);
+           char c = s.charAt(i);
             
-            // If digit calculate it on the one go and deal with it immediately
+           if (c == ' ') continue;
+           
+           if (Character.isDigit(c)) {
+               
+               // get the entire digit at once and dump in the stack
+               int val = 0;
+               
+               while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                   val = val * 10 + (s.charAt(i) - '0');
+                   i++;
+               }
+               i--;
+               
+               if (sign == '+') {
+                   st.add(val);
+               } else if (sign == '-') {
+                   st.add(-val);
+               } else if (sign == '*') {
+                   st.add(st.pop() * val);
+               } else if (sign == '/') {
+                   st.add(st.pop() / val);
+               }
+               
+           } else {
+               // char is not a digit
+               sign = c;
+               
+           }
             
-            if (Character.isDigit(c)) {
-                int num = 0;               
-                while ( i < s.length() && Character.isDigit(s.charAt(i))) {
-                    num = num * 10 + (s.charAt(i) - '0');
-                    i++;
-                }
-                i--;
-                
-                // lets see what to do with this val
-                if (lastsign == '+') {
-                    st.add(num);
-                } else if (lastsign == '-') {
-                    st.add(-num);
-                } else if (lastsign == '*') {
-                    st.add(st.pop() * num);
-                } else if (lastsign == '/') {
-                    st.add(st.pop() / num);
-                }
-                
-            }
-            else if (c != ' ') {
-               lastsign = c; 
-            }            
-
+            
         }
         
         int result = 0;
-        while (!st.isEmpty()) {
-            result += st.pop();
-        }
+        
+        while (!st.isEmpty()) result += st.pop();
         
         return result;
     }

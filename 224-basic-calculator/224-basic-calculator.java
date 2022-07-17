c6819ca25c0a71ff0c4ba89bcef45565b1341c6d
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-17 03:37:20 -0400

    Time: 13 ms (60.21%), Space: 44 MB (74.97%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,52 +1,45 @@
 class Solution {
-    public int calculate(String s) {        
-        // since this has () -> we need to use stack       
-        s = s.trim();
-        
+    public int calculate(String s) {
+
         int result = 0;
         int num = 0;
         int sign = 1;
         
-        Stack<Integer> st = new Stack<>();        
+        Stack<Integer> st = new Stack<>();
         
         for (int i = 0; i < s.length(); i++) {
             
             char c = s.charAt(i);
             
             if (Character.isDigit(c)) {
-               num = num * 10 + c - '0'; 
+                num = num * 10 + c - '0';
             } else if (c == '+') {
                 result += sign * num;
-                num = 0;
                 sign = 1;
+                num = 0;
             } else if (c == '-') {
                 result += sign * num;
-                num = 0;
                 sign = -1;
+                num = 0;
             } else if (c == '(') {
-                // store result and sign into the stack -> we will use it later
                 st.add(result);
                 st.add(sign);
-                
-                // reset them
                 result = 0;
-                sign = 1;
-                num = 0;
+                sign = 1;              
             } else if (c == ')') {
-                
                 result += sign * num;
-                num = 0;
                 sign = 1;
+                num = 0;
                 
-                // get the previous result and sign from the stack and merge it
+                // use previous sign and result
                 result *= st.pop();
                 result += st.pop();
             }
-         
-            }
-            // add final digit if there
-            result += sign * num;
-             return result;
+            
         }
         
+        // process final digit in expression
+        result += sign * num;
+        return result;
+    }
 }

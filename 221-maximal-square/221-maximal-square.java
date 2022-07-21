a1b4c0a7e0cfa83cce5367e1a68b11cd66e9330d
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-21 09:13:23 -0400

    Time: 9 ms (51.12%), Space: 58.4 MB (48.12%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -2,45 +2,27 @@ class Solution {
     public int maximalSquare(char[][] matrix) {
         
         if (matrix == null || matrix.length == 0) return 0;
+        
         int m = matrix.length;
         int n = matrix[0].length;
         
-        // Initial Max Size
+        // This is DP Array 
+        // i, j will represent the corresponding max square size
+        // we will copy 1st row and col values from original matrix to dp
+        int[][] dp = new int[m + 1][n + 1];
         int max = 0;
         
-        // Move through the matrix
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++){
-                if (matrix[i][j] == '1') {
-                    int l = 1;
-                    boolean flag = true;
-                    while (i + l < m && j + l < n && flag) {
-                        
-                        // check all the row elements
-                        for (int k = j + l; k >= j; k--) {
-                            if (matrix[i + l][k] == '0') {
-                                flag = false;
-                                break;
-                            }
-                        }
-                        
-                        // check all the col elements
-                        for (int k = i + l; k >= i; k--) {
-                            if (matrix[k][j + l] == '0') {
-                                flag = false;
-                                break;
-                            }
-                        }
-                        
-                     if (flag) l++;
-                        
-                    }
-                    
-                    max = Math.max(max, l);
-          }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
             }
         }
         
         return max * max;
+        
+        
     }
 }

0367b184ea9525c6ef71686fefd82831abc83a2b
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-23 01:53:50 -0400

    Time: 2 ms (30.78%), Space: 43.6 MB (54.96%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,30 +1,24 @@
 class Solution {
     
-    private int tollPaid(int[] cost, int[] dp, int n) {
+    public int helper(int[] cost, int[] dp, int n) {
         
-      // You can either start from the step with index 0, or the step with index 1.
-      if (n < 0) return 0;
-      if (n == 0 || n == 1) return cost[n];
+        if (n < 0) return 0;
+        if (n == 0 || n == 1) return cost[n];
         
-      if (dp[n] != -1) return dp[n];
+        if (dp[n] != -1) return dp[n];
         
-      return dp[n] = cost[n] + Math.min(tollPaid(cost, dp, n - 1), tollPaid(cost, dp, n - 2));
-        
-        
-        
+        return dp[n] = cost[n] + Math.min(helper(cost, dp, n - 1), helper(cost, dp, n - 2));
     }
-    
     public int minCostClimbingStairs(int[] cost) {
         
-        // here the n lies beyonfd the array length
-        // we need to reach n
+        // This is classic problem
         int n = cost.length;
         
         int[] dp = new int[n + 1];
         
         Arrays.fill(dp, -1);
-        // we need to pay the toll till n - 1 or n-2 and Jump from either of them\
-        return Math.min(tollPaid(cost, dp, n - 1), tollPaid(cost, dp, n - 2));
         
+        return Math.min(helper(cost, dp, n - 1), helper(cost, dp, n - 2));
+         
     }
 }

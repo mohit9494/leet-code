6ee4e995f9375beb90e7692f9742e411f29c1d85
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-27 01:01:48 -0400

    Time: 3 ms (24.00%), Space: 75.2 MB (27.58%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,20 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         
+        if (nums.length == 1) return nums[0];
         
-        int maxi = Integer.MIN_VALUE;
         int sum = 0;
+        int maxi = Integer.MIN_VALUE;
         
         for (int i = 0; i < nums.length; i++) {
-            
-            sum = sum + nums[i];
-            
-            maxi = Math.max(maxi, sum);
-            
-            // Kadane's
-            if (sum < 0) sum = 0;                  
+            // Get the effective running sum;
+            sum = Math.max(sum + nums[i], nums[i]);
+            maxi = Math.max(sum, maxi);
         }
         
-        return maxi;        
+        return maxi;
+        
     }
 }

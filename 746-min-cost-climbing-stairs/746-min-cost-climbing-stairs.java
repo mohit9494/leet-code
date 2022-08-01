f970b9edf1e7a1e875324471369f9983efb8be73
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-07-31 22:41:32 -0400

    Time: 1 ms (89.87%), Space: 43.8 MB (33.24%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,26 +1,19 @@
 class Solution {
     
-    public int helper(int[] cost, int n, int[] dp) {
-        
-        if (n == 0 || n == 1) return cost[n];
-        
-        if (dp[n] != -1) return dp[n];
-        
-        return dp[n] = cost[n] + Math.min(helper(cost, n - 1, dp), helper(cost, n - 2, dp));
-    }
-    
     public int minCostClimbingStairs(int[] cost) {
         
-      int n = cost.length; 
-      int[] dp = new int[n];
+      int n = cost.length;
         
-      dp[0] = cost[0];
-      dp[1] = cost[1];
+      int a = cost[0];
+      int b = cost[1];
+      int c = 0;
         
       for (int i = 2; i <= n - 1; i++) {
-          dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+           c = cost[i] + Math.min(b, a);
+          a = b;
+          b = c;
       }
        
-      return Math.min(dp[n - 1], dp[n - 2]);  
+      return Math.min(a, b);  
     }
 }

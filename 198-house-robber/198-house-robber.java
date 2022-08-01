9ff7935475dbb938959cd32a7e4775ac6176d500
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-01 03:28:33 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (24.64%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,22 +1,31 @@
 class Solution {
+    
+    public int helper(int[] nums, int index, int[] dp) {
+        
+        // if it choose the last element
+        if (index == nums.length - 1) return nums[index];
+        
+        // If it skips last and go beyond
+        if (index >= nums.length) return 0;
+        
+        if (dp[index] != -1) return dp[index];
+        
+        // pick the house
+        int pick = nums[index] + helper(nums, index + 2, dp);
+        // Not pick the house
+        int notPick = 0 + helper(nums, index + 1, dp);
+        
+        return dp[index] = Math.max(pick, notPick);
+        
+    }
+    
     public int rob(int[] nums) {
         
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        if (n == 1) return dp[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int[] dp = new int[n + 1];
         
-        for (int i = 2; i <= n - 1; i++) {
-            
-            // pick
-            int pick = nums[i] + dp[i - 2];
-            // not pick
-            int notPick = dp[i - 1];
-            
-            dp[i] = Math.max(pick, notPick);
-        }
-      
-        return dp[n - 1];
+        Arrays.fill(dp, -1);
+       return helper(nums, 0, dp);
+        
     }
 }

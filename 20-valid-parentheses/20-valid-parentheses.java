1629b0d318e4561230dd89d5308fc11e85c6edb6
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-03 05:01:36 -0400

    Time: 1 ms (99.61%), Space: 40.5 MB (89.17%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
         
+        if (s.length() % 2 != 0) return false;
+        
         Stack<Character> st = new Stack<>();
         
         for (char c : s.toCharArray()) {

e742aa2ff9bb51712169c81b1c9342b56c0522c7
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-03 04:03:29 -0400

    Time: 5 ms (52.06%), Space: 43.2 MB (88.04%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        
+        int n = ratings.length;
+        if (n == 1) return 1;
+        
+        int[] ans = new int[n];
+        Arrays.fill(ans, 1);
+        
+        // Move from left to end -> compare only left side and give candies
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                ans[i] = ans[i - 1] + 1; 
+            }
+        }
+        
+        // Move from right to left -> Careful :: don't change already given value if new one is min
+        // to maintain relative weight
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                ans[i] = Math.max(ans[i], ans[i + 1] + 1);
+            }
+        }
+        
+        // array is filled
+        return Arrays.stream(ans).sum();
+    }
+}

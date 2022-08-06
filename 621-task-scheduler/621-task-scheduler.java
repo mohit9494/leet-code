8c8e2129f824043c3c63e76e101926d858927dec
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-05 22:57:18 -0400

    Time: 30 ms (50.03%), Space: 63.5 MB (43.50%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        
+        // Frequency Map
+        Map<Character, Integer> map = new HashMap<>();
+        
+        int maxF = 0; // MAX frequency
+        int maxFE = 0; // Total Number of elements with Max Frequency - we will group them 
+        for (char c : tasks) {
+           int count =  map.getOrDefault(c, 0);
+            count++;
+            map.put(c, count);
+            maxF = Math.max(maxF, count);
+        }
+        
+        for (char c : map.keySet()) {
+            if (map.get(c) == maxF) maxFE++;
+        }
+        
+        // Mathematics
+        int k = n - (maxFE - 1); // This is effective n
+        if (k < 0) k = 0;
+        
+        int slots = (maxF - 1) * k; // total slots
+        int pendingElements = tasks.length - (maxFE * maxF);
+        int Idle = Math.max(0, (slots - pendingElements));
+        int ans = tasks.length + Idle;
+        return ans;
+    }
+}

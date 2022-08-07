c82fe18276dd88dcbd1bc7f331f0a58fe50e223b
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-06 22:06:33 -0400

    Time: 1 ms (100.00%), Space: 42.7 MB (96.01%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -2,26 +2,17 @@ class Solution {
     
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        String[] dp =  new String[n];
-        Arrays.fill(dp, "U");
+        int lastIndex = n - 1;
         
-        // fill the last element with Good
-        dp[n - 1] = "G";
-        
-        for (int i = n - 2; i >= 0; i--) {
-           int farJump = Math.min(n - 1, i + nums[i]);
-           
-            for (int j = farJump; j >= i + 1; j--) {
-                if(dp[j] == "G") {
-                    dp[i] = "G";
-                    break;
-                }
-            }
-            dp[i] = dp[i] == "U" ? "B" : "G";
+        for (int i = n - 1; i >= 0; i--) {
+      
+           if (nums[i] + i >= lastIndex) {
+               lastIndex = i;
+           } 
+            
         }
         
-        System.out.println(Arrays.toString(dp));
         
-     return dp[0] == "G";   
+     return lastIndex == 0 ;   
     }
 }

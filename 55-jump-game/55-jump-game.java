08409304d4cb2f11fef464edb068fa5951577dc1
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-06 21:45:44 -0400

    Time: 160 ms (24.46%), Space: 69.9 MB (6.52%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,29 +1,24 @@
 class Solution {
     
-    String[] dp;
-    
-    public boolean canJumpfrom(int position, int[] nums) {
-        if (position == nums.length - 1) return true;
-        
-        if (!dp[position].equals("U")) return dp[position] == "G" ? true : false;
-        
-        int farJump = Math.min(position + nums[position], nums.length - 1);
-        
-        for (int i = farJump; i >= position + 1; i--) {
-            if (canJumpfrom(i, nums)) {
-                dp[position] = "G";
-                return true;
-            }
-        }
-        dp[position] = "B";
-        return false;
-    }
-    
     public boolean canJump(int[] nums) {
-        this.dp =  new String[nums.length];
-        Arrays.fill(this.dp, "U");
-       boolean result =  canJumpfrom(0, nums);
-        System.out.println(Arrays.toString(this.dp));
-        return result;
+        int n = nums.length;
+        String[] dp =  new String[n];
+        Arrays.fill(dp, "U");
+        
+        // fill the last element with Good
+        dp[n - 1] = "G";
+        
+        for (int i = n - 2; i >= 0; i--) {
+           int farJump = Math.min(n - 1, i + nums[i]);
+           
+            for (int j = farJump; j >= i + 1; j--) {
+                if(dp[j] == "G") {
+                    dp[i] = "G";
+                    break;
+                }
+            }
+            dp[i] = dp[i] == "U" ? "B" : "G";
+        }
+     return dp[0] == "G";   
     }
 }

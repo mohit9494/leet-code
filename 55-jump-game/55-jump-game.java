e4f4f7574095c8103203bb65739cbc96667239f4
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-06 21:27:09 -0400

    Time: 625 ms (10.74%), Space: 42.8 MB (92.47%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    String[] dp;
+    
+    public boolean canJumpfrom(int position, int[] nums) {
+        if (position == nums.length - 1) return true;
+        
+        if (!dp[position].equals("U")) return dp[position] == "G" ? true : false;
+        
+        int farJump = Math.min(position + nums[position], nums.length - 1);
+        
+        for (int i = farJump; i >= position + 1; i--) {
+            if (canJumpfrom(i, nums)) {
+                dp[position] = "G";
+                return true;
+            }
+        }
+        dp[position] = "B";
+        return false;
+    }
+    
+    public boolean canJump(int[] nums) {
+        this.dp =  new String[nums.length];
+        Arrays.fill(this.dp, "U");
+       return canJumpfrom(0, nums); 
+    }
+}

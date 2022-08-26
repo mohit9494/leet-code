b1c89cf197858fba59ab86d7e1698f10feed9844
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-25 23:52:23 -0400

    Time: 19 ms (5.30%), Space: 51.4 MB (5.19%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,39 +1,33 @@
 class Solution {
+    
     public int minFallingPathSum(int[][] matrix) {
         
-        int r = matrix.length;
-        int c = matrix[0].length;
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
         
-        int[][] dp = new int[r][c];
-        
-        // Initialize the dp array
-        for (int j = 0; j < c; j++) {
-            dp[r -1][j] = matrix[r - 1][j];
+        // Initialization
+        for (int k = 0; k < n; k++) dp[n - 1][k] = matrix[n - 1][k];
+                                                 
+        for (int i = n - 2; i >=0 ; i--) {
+            for (int j = 0; j < n; j++) {
+                int val = matrix[i][j];
+                int l = Integer.MAX_VALUE;
+                int r = Integer.MAX_VALUE;
+                if (j - 1 >= 0) l = val + dp[i + 1][j - 1];
+                if (j + 1 <= n - 1) r = val + dp[i + 1][j + 1];
+                int c = val + dp[i + 1][j];
+                
+                dp[i][j] = Math.min(l, Math.min(c, r));
+            }
         }
         
-        for (int i = r - 2; i >= 0; i--) {
-            for (int j = 0; j < c; j++) {
-                
-                int a = j == 0 ? Integer.MAX_VALUE : dp[i + 1][j - 1];
-                int b = dp[i+ 1][j];
-                int z = j == c - 1 ? Integer.MAX_VALUE : dp[i+ 1][j + 1];
-                
-                int min = Math.min(a, Math.min(b,z));
-             
-                dp[i][j] = min + matrix[i][j];
-                
-            }          
-            
-        }
-        
-        int ans = Integer.MAX_VALUE;
-        
-        // Get the min from the first row
-        for (int j = 0; j < c; j++) {
-            
-            ans = Math.min(ans, dp[0][j]);
-        }
-        
-        return ans;
+        System.out.println(Arrays.deepToString(dp));
+         
+         int min = Integer.MAX_VALUE;
+         for (int z = 0; z < n ; z++) {
+             min = Math.min(min, dp[0][z]);
+         }
+         return min;                                                  
+                                                  
     }
 }

836592674304f306f663d2bcb6c005ad731800e0
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-08-26 00:21:58 -0400

    Time: 31 ms (5.01%), Space: 49.6 MB (5.19%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -3,31 +3,38 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         
         int n = matrix.length;
-        int[][] dp = new int[n][n];
+        int[] prev = new int[n];
+        int[] curr = new int[n];
+        
+        prev = matrix[n - 1];
+        
+        if (n == 1) return matrix[0][0];
         
         // Initialization
-        for (int k = 0; k < n; k++) dp[n - 1][k] = matrix[n - 1][k];
+        // for (int k = 0; k < n; k++) dp[n - 1][k] = matrix[n - 1][k];
                                                  
         for (int i = n - 2; i >=0 ; i--) {
+            curr = new int[n];
             for (int j = 0; j < n; j++) {
                 int val = matrix[i][j];
                 int l = Integer.MAX_VALUE;
                 int r = Integer.MAX_VALUE;
-                if (j - 1 >= 0) l = val + dp[i + 1][j - 1];
-                if (j + 1 <= n - 1) r = val + dp[i + 1][j + 1];
-                int c = val + dp[i + 1][j];
+                if (j - 1 >= 0) l = val + prev[j - 1];
+                if (j + 1 <= n - 1) r = val + prev[j + 1];
+                int c = val + prev[j];
                 
-                dp[i][j] = Math.min(l, Math.min(c, r));
+                curr[j] = Math.min(l, Math.min(c, r));
             }
+            prev = curr;
+            System.out.println(Arrays.toString(curr));
         }
         
-        System.out.println(Arrays.deepToString(dp));
+        //System.out.println(Arrays.deepToString(dp));
          
          int min = Integer.MAX_VALUE;
          for (int z = 0; z < n ; z++) {
-             min = Math.min(min, dp[0][z]);
+             min = Math.min(min, curr[z]);
          }
-         return min;                                                  
-                                                  
+         return min;                                                                                                   
     }
 }

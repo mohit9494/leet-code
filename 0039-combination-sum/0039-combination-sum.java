c3beff16977d6a53083ed06c5f72778cb7f93826
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-19 00:38:57 -0400

    Time: 4 ms (79.90%), Space: 45.3 MB (38.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -26,6 +26,7 @@ class Solution {
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
+        Arrays.sort(candidates);
         
         helper(candidates, target, new ArrayList<>(), 0);
         

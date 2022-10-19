20e2f23e0816f6847c54266589047982c9cf7c81
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-19 00:38:12 -0400

    Time: 4 ms (79.90%), Space: 44.2 MB (82.19%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,27 +2,32 @@ class Solution {
     
     List<List<Integer>> result = new ArrayList<>();
     
-    public void helper(int[] a, int target, List<Integer> ds, int index, int n) {
+    public void helper(int[] a, int target, List<Integer> ds, int index) {
         
-            if (target < 0 || index >= n ) return;
-            if (target == 0) {
+        if (index == a.length) {
+          if (target == 0) {
                 result.add(new ArrayList<>(ds));
-                return;
-            }            
+            } 
+            return;
+        }
+                      
         
         // select unlimited
+        if (a[index] <= target) {
         ds.add(a[index]);
-        helper(a, target - a[index], ds, index, n);
-        ds.remove(ds.size() - 1);
+        helper(a, target - a[index], ds, index);
+        ds.remove(ds.size() - 1);   
+        }
+       
         
         // Not select 
-        helper(a, target, ds, index + 1, n);        
+        helper(a, target, ds, index + 1);        
     }
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
         
-        helper(candidates, target, new ArrayList<>(), 0, candidates.length);
+        helper(candidates, target, new ArrayList<>(), 0);
         
         return result;
         

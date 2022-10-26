ec3a017f07eb061866d7258881aa4ea00bbc335e
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-26 04:00:25 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (35.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,20 +3,21 @@ class Solution {
     public int rob(int[] nums) { 
         int n = nums.length;
         if (n == 1) return nums[0];
-        int[] dp = new int[n + 1];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        
+        int a = nums[0];
+        int b = Math.max(nums[0], nums[1]);
+        int c = b;
         for (int i = 2; i <= n - 1; i++) {
             // pick
-            int pick = nums[i] + dp[i - 2];
+            int pick = nums[i] + a;
             
             // not pick
-            int notPick = dp[i - 1];
+            int notPick = b;
             
-            dp[i] =  Math.max(pick, notPick);
+            c =  Math.max(pick, notPick);
+            a = b;
+            b = c;
         }
             
-        return dp[n - 1];
+        return c;
     }
 }

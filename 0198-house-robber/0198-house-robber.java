f05aa8383640bf37d795a8ab1c8269325e69032c
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-26 03:26:37 -0400

    Time: 0 ms (100.00%), Space: 39.5 MB (96.67%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public int helper(int[] nums, int index, int[] dp) {
+        
+        if (index == nums.length - 1) {
+            return nums[index];
+        } else if (index >= nums.length) {
+            return 0;
+        }
+        if (dp[index] != -1) return dp[index];
+        
+        // pick
+       int left =  nums[index] + helper(nums, index + 2, dp);
+        
+        // Not pick
+       int right =  helper(nums, index + 1, dp);
+        
+       return dp[index] = Math.max(left, right);
+    }
+    
+    public int rob(int[] nums) { 
+        int[] dp = new int[nums.length + 1];
+        Arrays.fill(dp, -1);
+        return helper(nums, 0, dp);
+    }
+}

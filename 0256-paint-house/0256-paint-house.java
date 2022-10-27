f1a82b3695726cedae7b62ef7b187b0d5cd72d30
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-26 23:40:51 -0400

    Time: 1 ms (95.09%), Space: 41.5 MB (97.95%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -1,29 +1,27 @@
 class Solution {
     
-    public int helper(int[][] cost, int house, int past, int[][] dp) {
+    public int helper(int[][] costs, int house, int last, int[][] dp) {
         
-        if (house == cost.length) return 0;
+        if (house == costs.length) return 0;
         
-        if (dp[house][past] != -1) return dp[house][past];
+        if (dp[house][last] != -1) return dp[house][last];
         
-        // select min cost by excluding past and trying all
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i <= 2; i++) {
-           if (i == past) continue;
-           int value = cost[house][i] + helper(cost, house + 1, i, dp);
-           min = Math.min(min,value); 
+        
+        for (int i = 0; i <= 2 ; i++) {
+          if (i == last) continue;
+          min = Math.min(min, costs[house][i] + helper(costs, house + 1, i, dp));  
         }
-        return dp[house][past] = min;
+        
+        return dp[house][last] = min;
     }
     
-    public int minCost(int[][] costs) {
-        
-        // pass costs, house number and last selected
-        // for first one it will be 3
-        // only two param change - house and color
-        int[][] dp = new int[costs.length + 1][4];
+    public int minCost(int[][] costs) { 
+        int n = costs.length;
+        int m = costs[0].length;
+        int[][] dp = new int[n][m + 1]; // 4 values are there
         for (int[] arr : dp) Arrays.fill(arr, -1);
         return helper(costs, 0, 3, dp);
-        
     }
+    
 }

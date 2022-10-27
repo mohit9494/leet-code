26666a3ad2a1d37d4be2ae3a97dad2e9b6f8d134
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-27 00:19:08 -0400

    Time: 7 ms (12.42%), Space: 43.6 MB (35.02%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -5,23 +5,29 @@ class Solution {
         int n = costs.length;
         int m = costs[0].length;
         
+        int[] previous = costs[n - 1];
+        
         for (int i = n - 2; i >= 0; i--) {
+            
+           int[] current = costs[i].clone();
+            
            for (int j = 0; j < m; j++) {
-             
+
                // calculate the min such that 
                // it will exclude the j which is previous color
                int min = Integer.MAX_VALUE;
                for (int k = 0; k < m; k++) {
                    if (k == j) continue;
-                   min = Math.min(min, costs[i + 1][k]);
+                   min = Math.min(min, previous[k]);
                }
                
-               costs[i][j] = costs[i][j] + min;
-           }    
+               current[j] = current[j] + min;
+           } 
+            previous = current;
         }
         
         // answer will be in the first row of costs
-        return Arrays.stream(costs[0]).min().getAsInt();
+        return Arrays.stream(previous).min().getAsInt();
     }
     
 }

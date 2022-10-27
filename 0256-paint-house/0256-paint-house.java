4f47c4bf4845a124f00f3d0ac794f6f2bbf87b52
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-27 00:01:01 -0400

    Time: 7 ms (12.42%), Space: 43.7 MB (24.23%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -1,27 +1,27 @@
 class Solution {
     
-    public int helper(int[][] costs, int house, int last, int[][] dp) {
-        
-        if (house == costs.length) return 0;
-        
-        if (dp[house][last] != -1) return dp[house][last];
-        
-        int min = Integer.MAX_VALUE;
-        
-        for (int i = 0; i <= 2 ; i++) {
-          if (i == last) continue;
-          min = Math.min(min, costs[house][i] + helper(costs, house + 1, i, dp));  
-        }
-        
-        return dp[house][last] = min;
-    }
-    
     public int minCost(int[][] costs) { 
+        
         int n = costs.length;
         int m = costs[0].length;
-        int[][] dp = new int[n][m + 1]; // 4 values are there
-        for (int[] arr : dp) Arrays.fill(arr, -1);
-        return helper(costs, 0, 3, dp);
+        
+        for (int i = n - 2; i >= 0; i--) {
+           for (int j = 0; j < m; j++) {
+             
+               // calculate the min such that 
+               // it will exclude the j which is previous color
+               int min = Integer.MAX_VALUE;
+               for (int k = 0; k < m; k++) {
+                   if (k == j) continue;
+                   min = Math.min(min, costs[i + 1][k]);
+               }
+               
+               costs[i][j] = costs[i][j] + min;
+           }    
+        }
+        
+        // answer will be in the first row of costs
+        return Arrays.stream(costs[0]).min().getAsInt();
     }
     
 }

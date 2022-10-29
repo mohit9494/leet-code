38e04ff5d8651ed9bf2b288cb23f00415d1e5f1f
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-29 18:17:03 -0400

    Time: 3 ms (8.35%), Space: 41.5 MB (10.08%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -17,8 +17,17 @@ class Solution {
     
      int[][] dp = new int[m][n];
         
-     for(int[] arr : dp) Arrays.fill(arr, -1); 
+     for (int i = 0; i <= m - 1; i++) {
+         for (int j = 0; j <= n - 1; j++) {
+             if (i == 0 && j == 0) dp[i][j] = 1;
+             else {
+               int up = i - 1 >= 0 ? dp[i - 1][j] : 0;
+             int left = j - 1 >= 0 ? dp[i][ j - 1] : 0;
+             dp[i][j] = up + left;   
+             } 
+         }
+     }
         
-     return helper(m - 1, n - 1, dp);   
+     return dp[m - 1][n - 1];   
     }
 }

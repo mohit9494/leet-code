f51aa585e36abad236c435dc33af537be0506956
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-10-29 18:39:28 -0400

    Time: 1 ms (78.88%), Space: 42.2 MB (46.10%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    public int helper(int i, int j, int[][] arr, int[][] dp) {
+        
+        if (i == 0 && j == 0 && arr[i][j] != 1) return 1;
+        
+        if ( i < 0 || j < 0 || arr[i][j] == 1) return 0;
+        
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        return dp[i][j] = helper(i - 1, j, arr, dp) + helper(i, j - 1, arr, dp);
+        
+    }
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n]; 
+        for (int[] arr : dp) Arrays.fill(arr, -1);
+        return helper(m - 1, n - 1, obstacleGrid, dp);
+    }
+}

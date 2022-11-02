adc7b695246243417295537262b372979dec731d
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-02 04:25:54 -0400

    Time: 10 ms (85.03%), Space: 42.7 MB (51.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,27 @@
 class Solution {
-    
-    public int helper(int index, int target, int[] arr) {
+    // used as an offset
+    int total;
+    public int helper(int index, int target, int[] arr, int[][] dp, int sum) {
         
        if (index < 0) {
-           if (0 == target) return 1;
+           if (sum == target) return 1;
            else return 0;
        }
+       
+       if (dp[index][sum + total] != Integer.MIN_VALUE) return dp[index][sum + total]; 
         
-       int plus = helper(index - 1, target - arr[index], arr);
-       int minus = helper(index - 1, target + arr[index], arr);
+       int plus = helper(index - 1, target, arr, dp, sum + arr[index]);
+       int minus = helper(index - 1, target, arr, dp, sum - arr[index]);
     
-       return plus + minus;
+       return dp[index][sum + total] = plus + minus;
     }
     
     public int findTargetSumWays(int[] nums, int target) {
       
         int n = nums.length;
-        return helper(n - 1, target, nums);
+        total = Arrays.stream(nums).sum();
+        int[][] dp = new int[n + 1][2 * total + 1];
+        for (int[] arr : dp) Arrays.fill(arr, Integer.MIN_VALUE);
+        return helper(n - 1, target, nums, dp, 0);
     }
 }

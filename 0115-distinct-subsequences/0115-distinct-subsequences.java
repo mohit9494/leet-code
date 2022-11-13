98afdb03a934521cc3aa94c3a82586031b69ff6c
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-13 16:56:46 -0500

    Time: 29 ms (68.50%), Space: 50.3 MB (43.40%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,12 +3,12 @@ class Solution {
     public int helper(int i, int j, String s, String t, int[][] dp) {
         
         // base case
-        if (j < 0) return 1;
-        if (i < 0) return 0;
+        if (j == 0) return 1;
+        if (i == 0) return 0;
         
         if (dp[i][j] != -1) return dp[i][j];
         // if match -> either select or not select (not interested)
-        if (s.charAt(i) == t.charAt(j)) {
+        if (s.charAt(i - 1) == t.charAt(j - 1)) {
           return dp[i][j]= helper(i - 1, j - 1, s, t, dp) + helper(i - 1, j, s, t, dp);
         } else {
           return dp[i][j]= helper(i - 1, j,s, t, dp);
@@ -19,8 +19,8 @@ class Solution {
         
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n + 1][m + 1];
         for (int[] arr : dp) Arrays.fill(arr, -1);
-        return helper(n - 1, m - 1, s, t, dp);
+        return helper(n, m, s, t, dp);
     }
 }

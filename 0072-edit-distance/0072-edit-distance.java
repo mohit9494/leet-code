9fb11dc5453cb13b37fd5760d5adf1360ec09012
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-13 17:54:01 -0500

    Time: 17 ms (10.52%), Space: 45.4 MB (11.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,24 +1,5 @@
 class Solution {
     
-    public int helper(int i, int j, String s, String t, int[][] dp) {
-        
-        // base case
-        if (i < 0) return j + 1;
-        if (j < 0) return i + 1;
-        if (dp[i][j] != -1) return dp[i][j];
-        // if match
-        if (s.charAt(i) == t.charAt(j)) {
-            return dp[i][j] = helper(i - 1, j - 1, s, t, dp); 
-        }
-        
-        // if no match - perform either insert, delete or replace and get min
-        int in = 1 + helper(i, j - 1, s, t, dp);
-        int de = 1 + helper(i - 1, j, s, t, dp);
-        int re = 1 + helper(i - 1, j - 1, s, t, dp);
-        
-        return dp[i][j] = Math.min(in, Math.min(de, re));
-    }
-    
     public int minDistance(String word1, String word2) {
        
       int n = word1.length();
@@ -30,11 +11,10 @@ class Solution {
       for (int j = 0; j <= m; j++) dp[0][j] = j;
         
       for (int i = 1; i <= n; i++) {
-          for (int j = 1; j <= m; j++) {
-              
-        // if match
-        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-          dp[i][j] = dp[i - 1][j - 1]; 
+          for (int j = 1; j <= m; j++) {           
+            // if match
+         if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+              dp[i][j] = dp[i - 1][j - 1]; 
         } else {
              // if no match - perform either insert, delete or replace and get min
             int in = 1 + dp[i][j - 1];

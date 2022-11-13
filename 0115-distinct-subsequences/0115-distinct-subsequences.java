a237042e84ce7345c06aebbb7329ef85bc66414d
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-13 17:01:17 -0500

    Time: 41 ms (31.76%), Space: 50.7 MB (15.67%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,26 +1,25 @@
 class Solution {
     
-    public int helper(int i, int j, String s, String t, int[][] dp) {
-        
-        // base case
-        if (j == 0) return 1;
-        if (i == 0) return 0;
-        
-        if (dp[i][j] != -1) return dp[i][j];
-        // if match -> either select or not select (not interested)
-        if (s.charAt(i - 1) == t.charAt(j - 1)) {
-          return dp[i][j]= helper(i - 1, j - 1, s, t, dp) + helper(i - 1, j, s, t, dp);
-        } else {
-          return dp[i][j]= helper(i - 1, j,s, t, dp);
-        }             
-    }
-    
     public int numDistinct(String s, String t) {
         
         int n = s.length();
         int m = t.length();
+        // right shifting the index by 1
         int[][] dp = new int[n + 1][m + 1];
-        for (int[] arr : dp) Arrays.fill(arr, -1);
-        return helper(n, m, s, t, dp);
+        
+        // Initialization
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j<= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                   dp[i][j]= dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                   dp[i][j]= dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 }

099469f418dc90d3bd7c3d585c3a9a121b1f2579
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-20 14:00:46 -0500

    Time: 2 ms (92.88%), Space: 59.2 MB (84.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,9 +2,7 @@ class Solution {
     public int maxProfit(int[] arr) {
        
         int n = arr.length;
-        int p = 0;    
-        if (n == 1) return p;
-        
+        int p = 0;        
         int min = arr[0];
         
         for (int i = 1; i <= n - 1; i++) {
@@ -12,9 +10,7 @@ class Solution {
                 p = Math.max(p, arr[i] - min);
             }
             min = Math.min(min, arr[i]);
-        }
-        
- 
+        }         
         return p;
     }
 }

f261398d0e490c8c11330b2641fd175a0b3a4ca4
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-20 19:30:29 -0500

    Time: 249 ms (21.45%), Space: 124.4 MB (24.87%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -19,8 +19,41 @@ class Solution {
     
     public int maxProfit(int[] arr) {
         int cap = 2;
-        int[][][] dp = new int[arr.length][2][cap + 1];
-        for (int[][] x : dp) for (int[] y : x) Arrays.fill(y, -1);
-        return helper(0, 1, cap, arr, dp);
+        int n = arr.length;
+        int[][][] dp = new int[n + 1][2][cap + 1];
+        
+        // Initialization
+        // if cap == 0 
+        // no need since default val is 0
+//         for (int i = 0; i <= n - 1; i++) {
+//             for (int buy = 0; buy <= 1; buy++) {
+//                 dp[i][buy][0] = 0;
+//             }
+//         }
+        
+//         for (int buy = 0; buy <= 1; buy++){
+//             for (int c = 0; c <= 2; c++){
+//                 dp[n][buy][c] = 0;
+//             }
+//         }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1 ; buy++) {
+                for (int c = 1; c <= cap; c++) {
+                    
+                    int profit = 0;
+                    if (buy == 1) {
+                        profit = Math.max(-arr[i] + dp[i + 1][0][c], 0 + dp[i + 1][1][c]);
+                    } else {
+                        // once sell is complete 1 transaction is complete
+                        profit = Math.max(arr[i] + dp[i + 1][1][c - 1], 0 + dp[i + 1][0][c]);
+                    }        
+                     dp[i][buy][c] = profit;
+                    
+                }
+            }
+        }
+        
+        return dp[0][1][cap];
     }
 }

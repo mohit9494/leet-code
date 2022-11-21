9d0bccb8fc20d149ceb12036998c589ba5e17feb
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-20 19:37:25 -0500

    Time: 11 ms (55.67%), Space: 75.9 MB (81.62%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,41 +1,10 @@
 class Solution {
     
-    public int helper(int i, int buy, int cap, int[] arr, int[][][] dp) {
-        
-        if (cap == 0) return 0;
-        if (i == arr.length) return 0;
-        
-        if (dp[i][buy][cap] != -1) return dp[i][buy][cap];
-        
-        int profit = 0;
-        if (buy == 1) {
-            profit = Math.max(-arr[i] + helper(i + 1, 0, cap, arr, dp), 0 + helper(i + 1, 1, cap, arr, dp));
-        } else {
-            // once sell is complete 1 transaction is complete
-            profit = Math.max(arr[i] + helper(i + 1, 1, cap - 1, arr, dp), 0 + helper(i + 1, 0, cap, arr, dp));
-        }        
-        return dp[i][buy][cap] = profit;
-    }
-    
     public int maxProfit(int[] arr) {
         int cap = 2;
         int n = arr.length;
-        int[][][] dp = new int[n + 1][2][cap + 1];
-        
-        // Initialization
-        // if cap == 0 
-        // no need since default val is 0
-        for (int i = 0; i <= n - 1; i++) {
-            for (int buy = 0; buy <= 1; buy++) {
-                dp[i][buy][0] = 0;
-            }
-        }
-        
-        for (int buy = 0; buy <= 1; buy++){
-            for (int c = 0; c <= 2; c++){
-                dp[n][buy][c] = 0;
-            }
-        }
+        int[][] after = new int[2][cap + 1];
+        int[][] curr = new int[2][cap + 1];
         
         for (int i = n - 1; i >= 0; i--) {
             for (int buy = 0; buy <= 1 ; buy++) {
@@ -43,17 +12,17 @@ class Solution {
                     
                     int profit = 0;
                     if (buy == 1) {
-                        profit = Math.max(-arr[i] + dp[i + 1][0][c], 0 + dp[i + 1][1][c]);
+                        profit = Math.max(-arr[i] + after[0][c], 0 + after[1][c]);
                     } else {
                         // once sell is complete 1 transaction is complete
-                        profit = Math.max(arr[i] + dp[i + 1][1][c - 1], 0 + dp[i + 1][0][c]);
+                        profit = Math.max(arr[i] + after[1][c - 1], 0 + after[0][c]);
                     }        
-                     dp[i][buy][c] = profit;
-                    
+                    curr[buy][c] = profit;               
                 }
             }
+            after = curr;
         }
         
-        return dp[0][1][cap];
+        return curr[1][cap];
     }
 }

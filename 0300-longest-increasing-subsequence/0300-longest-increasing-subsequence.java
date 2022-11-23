95eaafaffc7336cb4568b136ab4b6a3b4c467b72
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-22 22:14:31 -0500

    Time: 78 ms (54.83%), Space: 45.1 MB (19.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,20 @@
 class Solution {
     
     public int lengthOfLIS(int[] arr) {
-       // consider using previous index and current index 
-       // use right shifting for prev index since you cannot store -1  
-       int n = arr.length;
-    // i -> n - 1 to 0
-    // prev -> i - 1 to -1 // right shift prev by +1
-       int[] next = new int[n + 1];
-       int[] curr = new int[n + 1];
-      
-       for (int i = n - 1; i >= 0 ; i--) {
-           for (int prev = i - 1; prev >= -1; prev--) {
-               
-                // pick and non-pick
-                int pick = 0;
-
-                if (prev == -1 || arr[i] > arr[prev]) {
-                    pick = 1 + next[i + 1];
-                }
-
-                int np = 0 + next[prev + 1];
-
-               curr[prev + 1] = Math.max(pick, np);  
-           }
-           next = curr;
-       } 
         
-       return curr[-1 + 1]; 
+       int n = arr.length;   
+       int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int maxi = 1;
+        
+       for (int i = 0; i <= n - 1; i++) {
+           for (int prev = i - 1; prev >= 0; prev--) {
+               if (arr[i] > arr[prev]) {
+                   dp[i] = Math.max(dp[i], 1 + dp[prev]);
+               }
+           }
+           maxi = Math.max(maxi, dp[i]);
+       }
+    return maxi;
     }
 }

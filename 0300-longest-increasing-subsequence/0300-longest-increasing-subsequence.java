b1c0e4cd07069f0797a676574f497b6c0eb62c7f
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-22 22:36:15 -0500

    Time: 71 ms (62.91%), Space: 42.1 MB (85.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,17 +4,35 @@ class Solution {
         
        int n = arr.length;   
        int[] dp = new int[n];
+        // for printing the longest common seq
+       int[] hash = new int[n];
         Arrays.fill(dp, 1);
         int maxi = 1;
-        
+        int lastIndex = 0;
        for (int i = 0; i <= n - 1; i++) {
            for (int prev = i - 1; prev >= 0; prev--) {
-               if (arr[i] > arr[prev]) {
-                   dp[i] = Math.max(dp[i], 1 + dp[prev]);
+               if (arr[i] > arr[prev] && 1 + dp[prev] > dp[i]) {
+                   dp[i] = 1 + dp[prev];
+                   hash[i] = prev;
                }
            }
-           maxi = Math.max(maxi, dp[i]);
+           
+           if (dp[i] > maxi) {
+               maxi = dp[i];
+               lastIndex = i;
+           }
        }
+        
+       List<Integer> ans = new ArrayList<>(); 
+       ans.add(0, arr[lastIndex]);
+       lastIndex = hash[lastIndex];
+        
+       while(lastIndex != hash[lastIndex]) {
+           ans.add(0, arr[lastIndex]);
+           lastIndex = hash[lastIndex];
+       }
+        
+     System.out.println(ans);
     return maxi;
     }
 }

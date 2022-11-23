48079934dc56366a02f3acb7c43080af1962fbae
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-22 21:25:23 -0500

    Time: 236 ms (12.28%), Space: 242.1 MB (5.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    public int helper(int prev, int i, int[] arr, int[][] dp) {
+    public int helper(int i, int prev, int[] arr, int[][] dp) {
         
         if (i >= arr.length) return 0;
         
@@ -9,20 +9,38 @@ class Solution {
         int pick = 0;
         
         if (prev == -1 || arr[i] > arr[prev]) {
-            pick = 1 + helper(i, i + 1, arr, dp);
+            pick = 1 + helper(i + 1, i, arr, dp);
         }
         
-        int np = 0 + helper(prev, i + 1, arr, dp);
+        int np = 0 + helper(i + 1, prev, arr, dp);
         
         return dp[i][prev + 1] = Math.max(pick, np);
     }
     
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS(int[] arr) {
        // consider using previous index and current index 
-       // use right shifting for prev index since you cannot store -1
-       int n = nums.length;
-       int[][] dp = new int[n][n + 1];
-       for (int[] k : dp) Arrays.fill(k , -1);
-       return helper(-1, 0, nums, dp); 
+       // use right shifting for prev index since you cannot store -1  
+       int n = arr.length;
+    // i -> n - 1 to 0
+    // prev -> i - 1 to -1
+       int[][] dp = new int[n + 1][n + 1];
+      
+       for (int i = n - 1; i >= 0 ; i--) {
+           for (int prev = i - 1; prev >= -1; prev--) {
+               
+                // pick and non-pick
+                int pick = 0;
+
+                if (prev == -1 || arr[i] > arr[prev]) {
+                    pick = 1 + dp[i + 1][i + 1];
+                }
+
+                int np = 0 + dp[i + 1][prev + 1];
+
+                dp[i][prev + 1] = Math.max(pick, np);  
+           }
+       } 
+        
+       return dp[0][-1 + 1]; 
     }
 }

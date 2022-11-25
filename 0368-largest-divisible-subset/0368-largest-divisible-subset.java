a1a371ebe6fea5a62ae83c01aecfa59675c97884
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-25 00:19:47 -0500

    Time: 21 ms (97.91%), Space: 42.5 MB (90.70%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] arr) {
+        
+        // this is similar to Longest Increaing Subseq
+        // we need to sort the array for divisibility
+        Arrays.sort(arr);
+        int n = arr.length;
+        int[] dp = new int[n];
+        int[] hash = new int[n];
+        int maxi = 1;
+        int lastIndex = 0;
+        
+        Arrays.fill(dp, 1);
+        
+        for (int i = 0; i <= n - 1; i++) {
+             hash[i] = i;
+            for (int prev = i - 1; prev >= 0; prev--) {
+                if (arr[i] % arr[prev] == 0 && dp[i] < 1 + dp[prev]) {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                }
+            }
+            if (maxi < dp[i]) {
+                maxi = dp[i];
+                lastIndex = i;
+            }
+        }
+        
+        // prepare the ans by jumping through indexes - hash 
+       List<Integer> ans = new ArrayList<>(); 
+       ans.add(arr[lastIndex]);
+        
+       while(lastIndex != hash[lastIndex]) {
+           lastIndex = hash[lastIndex];
+           ans.add(arr[lastIndex]);           
+       }
+
+        return ans;
+    }
+}

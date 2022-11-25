363a6ed14791c620f7e63743b7d51ece90ce1ed0
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-11-25 00:30:24 -0500

    Time: 73 ms (61.08%), Space: 44.7 MB (28.90%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -10,6 +10,7 @@ class Solution {
         int maxi = 1;
         int lastIndex = 0;
        for (int i = 0; i <= n - 1; i++) {
+           hash[i] = i;
            for (int prev = i - 1; prev >= 0; prev--) {
                if (arr[i] > arr[prev] && 1 + dp[prev] > dp[i]) {
                    dp[i] = 1 + dp[prev];
@@ -31,7 +32,7 @@ class Solution {
            ans.add(0, arr[lastIndex]);
        }
         
-      System.out.println(ans);
+      
 
     return maxi;
     }

42365cc163580783148b8307594d5c641d105724
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-12-03 23:57:58 -0500

    Time: 6 ms (66.80%), Space: 57.8 MB (28.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,41 +1,21 @@
- class Node {
-    int first;
-    int second;
-   public Node(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
+
 
 class Solution {
     
-    public void bfs(int row, int col, int[][] v, char[][] grid) {
+    public void dfs(int row, int col, int[][] v, char[][] grid) {
         
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(row, col));
+        if (row < 0 || row >= grid.length
+            || col < 0 || col >= grid[0].length || v[row][col] == 1 || grid[row][col] == '0') {
+            return ;
+        }
+        
         v[row][col] = 1;
         
-        int m = grid.length;
-        int n = grid[0].length;
-        
-        int[][] dir = {{0,1}, {0,-1}, {1,0},{-1,0}};
-        while (!q.isEmpty()) {
-            
-            Node node = q.poll();
-            int r = node.first;
-            int c = node.second;
-            
-            for (int k = 0; k < 4; k++) {
-                  int nr = r + dir[k][0];
-                   int nc = c + dir[k][1];
-                    if(nr >= 0 && nr < m && nc >= 0 && nc < n && 
-                       grid[nr][nc] == '1' && v[nr][nc] == 0) {                        
-                         v[nr][nc] = 1;
-                          q.add(new Node(nr, nc));                   
-                 }
-            }
-     
-        }
+        // Move into all 4 direction
+        dfs(row + 1, col, v, grid);
+        dfs(row - 1, col, v, grid);
+        dfs(row, col + 1, v, grid);
+        dfs(row, col - 1, v, grid);
         
     }
     
@@ -50,13 +30,11 @@ class Solution {
            // if I find land and not visited then that will be starting point
            if (grid[i][j] == '1' && v[i][j] == 0) {
              ans++;
-             bfs(i, j, v, grid);
+             dfs(i, j, v, grid);
            }
             
          } 
-      }
-       
-      
+      }   
       return ans;
     }
 }
